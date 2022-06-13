76bab7a81f03919f72a67a536f77f1f043a4d443
Nebby <nebby131999@gmail.com>
2022-06-12 22:13:58 -0400

    What if

--- SS2-Project/Packages/RoR2EditorKit/Editor/Common/TreedrawerCommon/DrawerLocator.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-//This just locates the state drawer in the project so it can reference the textures
-namespace RoR2EditorKit.Common
-{
-    public class DrawerLocator : ScriptableObject
-    {
-    }
-}
