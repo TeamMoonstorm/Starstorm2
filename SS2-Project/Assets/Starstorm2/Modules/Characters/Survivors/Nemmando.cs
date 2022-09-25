7e9cb541ca6b4060751965c2850b52a4c8a1f5c6
Anreol <12176932+Anreol@users.noreply.github.com>
2022-09-25 20:20:48 +0200

    sneedstorm

--- SS2-Project/Assets/Starstorm2/Modules/Characters/Survivors/Nemmando.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Characters/Survivors/Nemmando.cs
@@ -24,8 +24,9 @@ namespace Moonstorm.Starstorm2.Survivors
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         public void ScepterCompat()
         {
-            AncientScepter.AncientScepterItem.instance.RegisterScepterSkill(SS2Assets.LoadAsset<SkillDef>("NemmandoScepterSubmission"), "NemmandoBody", SkillSlot.Special, 0);
-            AncientScepter.AncientScepterItem.instance.RegisterScepterSkill(SS2Assets.LoadAsset<SkillDef>("NemmandoScepterBossAttack"), "NemmandoBody", SkillSlot.Special, 1);
+            //Counldn't give less of a fuck about ancient scepter
+            //AncientScepter.AncientScepterItem.instance.RegisterScepterSkill(SS2Assets.LoadAsset<SkillDef>("NemmandoScepterSubmission"), "NemmandoBody", SkillSlot.Special, 0);
+            //AncientScepter.AncientScepterItem.instance.RegisterScepterSkill(SS2Assets.LoadAsset<SkillDef>("NemmandoScepterBossAttack"), "NemmandoBody", SkillSlot.Special, 1);
         }
 
         public override void ModifyPrefab()
