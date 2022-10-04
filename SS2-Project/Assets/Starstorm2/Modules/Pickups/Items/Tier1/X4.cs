1091bccf5e624a82ea4cc4caa463a118550d5355
Lilith <zenithrium@gmail.com>
2022-10-04 17:55:01 -0400

    adds x4 and relic of force
    technically the model is assigned in the exec branch but its fine

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/X4.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/X4.cs
@@ -1,17 +1,34 @@
-﻿using RoR2;
+﻿using R2API;
+using RoR2;
 using RoR2.Items;
+using System;
 
 namespace Moonstorm.Starstorm2.Items
 {
-    [DisabledContent]
+    //[DisabledContent]
     
     public sealed class X4 : ItemBase
     {
         public override ItemDef ItemDef { get; } = SS2Assets.LoadAsset<ItemDef>("X4");
 
-        [ConfigurableField(ConfigDesc = "Bonus cooldown reduction per X-4 Stimulant.")]
+        [ConfigurableField(ConfigDesc = "Cooldown reduction per X-4 Stimulant. (1 = 1 second)")]
         [TokenModifier("SS2_ITEM_X4_DESC", StatTypes.Default, 0)]
         public static float secCooldown = 0.25f;
+
+        [ConfigurableField(ConfigDesc = "Percent healing upon activating a secondary skill. (1 = 1% of max health)")]
+        [TokenModifier("SS2_ITEM_X4_DESC", StatTypes.Percentage, 1)]
+        public static float percentHealth = .01f;
+
+        [ConfigurableField(ConfigDesc = "Percent healing per stack upon activating a secondary skill. (1 = 1% of max health)")]
+        [TokenModifier("SS2_ITEM_X4_DESC", StatTypes.Percentage, 2)]
+        public static float percentHealthStacking = .005f;
+
+        [ConfigurableField(ConfigDesc = "Flat healing upon activating a secondary skill. (1 = 1 health point)")]
+        [TokenModifier("SS2_ITEM_X4_DESC", StatTypes.Default, 3)]
+        public static float flatHealth = 20;
+
+        public static ProcChainMask ignoredProcs;
+
         public sealed class Behavior : BaseItemBodyBehavior, IStatItemBehavior
         {
             [ItemDefAssociation]
@@ -19,11 +36,61 @@ namespace Moonstorm.Starstorm2.Items
             public void RecalculateStatsEnd()
             {
                 if (body.skillLocator.secondaryBonusStockSkill)
-                    body.skillLocator.secondaryBonusStockSkill.flatCooldownReduction += stack * secCooldown;
+                {
+                    float skillCD = body.skillLocator.secondaryBonusStockSkill.baseRechargeInterval; //base cooldown of relevant skill
+                    float hyperbolicCDR = MSUtil.InverseHyperbolicScaling(secCooldown, secCooldown, skillCD - 1f, stack);
+                    float cdr;
+                    if(hyperbolicCDR > stack * secCooldown) //so hyperbolic is what i think we'd want here, but it vastly overpreforms with low stacks & and long cooldown. so i just take the lower value
+                    {
+                        cdr = stack * secCooldown;
+                    }
+                    else
+                    {
+                        cdr = hyperbolicCDR;
+                    }
+                    SS2Log.Debug("X4 RecalcStats end - " + hyperbolicCDR + " vs " + stack * secCooldown);
+                    body.skillLocator.secondaryBonusStockSkill.flatCooldownReduction += cdr;
+                }
+                //args.cooldownMultAdd += (float)itemCount * Synergies.pearlCD.Value;
                 //args.secondaryCooldownMultAdd *= MSUtil.InverseHyperbolicScaling(secCooldown, secCooldown, 0.7f, stack);
             }
             public void RecalculateStatsStart()
             {
+            }
+
+            //RecalculateStatsAPI.GetStatCoefficients += new RecalculateStatsAPI.StatHookEventHandler(Hook.RecalculateStatsAPI_GetStatCoefficients);
+            //public void ModifyStatArguments(RecalculateStatsAPI.StatHookEventArgs args)
+            //{
+            //    //float cdr = MSUtil.InverseHyperbolicScaling(secCooldown, secCooldown, 0.7f, stack);
+            //    //SS2Log.Debug("RecalcStats middle? - " + cdr);
+            //    //args.cooldownReductionAdd += stack * secCooldown;
+            //    
+            //}
+
+            private static void RecalculateStatsAPI_GetStatCoefficients(CharacterBody self, RecalculateStatsAPI.StatHookEventArgs args)
+            {
+            }
+            private void OnEnable()
+            {
+                On.RoR2.CharacterBody.OnSkillActivated += X4HealOnSkillActivation;
+            }
+
+            private void OnDisable()
+            {
+                On.RoR2.CharacterBody.OnSkillActivated -= X4HealOnSkillActivation;
+            }
+
+            private void X4HealOnSkillActivation(On.RoR2.CharacterBody.orig_OnSkillActivated orig, CharacterBody self, GenericSkill skill)
+            {
+                orig(self, skill);
+                if (skill.skillFamily.ToString().Contains("Secondary"))
+                {
+                    float amntToHeal = flatHealth + self.healthComponent.health * (percentHealth + (percentHealthStacking * (stack - 1)));
+                    self.healthComponent.Heal(amntToHeal, ignoredProcs, true);
+                }
+                //SS2Log.Debug("print list!: family: " + skill.skillFamily + " |  _family: " + skill._skillFamily + " |  " + skill.skillOverrides);
+                //if(skill.skill
+                //throw new NotImplementedException();
             }
         }
     }
--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Unfinished/RelicOfForce.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Unfinished/RelicOfForce.cs
@@ -4,10 +4,19 @@ using RoR2.Items;
 
 namespace Moonstorm.Starstorm2.Items
 {
-    [DisabledContent]
+    //[DisabledContent]
     public sealed class RelicOfForce : ItemBase
     {
         public override ItemDef ItemDef { get; } = SS2Assets.LoadAsset<ItemDef>("RelicOfForce");
+
+        [ConfigurableField(ConfigDesc = "Damage multipler per stack. (1 = 100% more damage, multiplicatively)")]
+        [TokenModifier("SS2_ITEM_RELICOFFORCE_DESC", StatTypes.Percentage, 0)]
+        public static float damageMultiplier = 1;
+
+        [ConfigurableField(ConfigDesc = "Attack speed reduction and cooldown increase per stack. (1 = 100% slower attack speed and longer cooldowns)")]
+        [TokenModifier("SS2_ITEM_RELICOFFORCE_DESC", StatTypes.Percentage, 1)]
+        public static float forcePenalty = .4f;
+
 
         public sealed class Behavior : BaseItemBodyBehavior, IBodyStatArgModifier
         {
@@ -16,9 +25,47 @@ namespace Moonstorm.Starstorm2.Items
 
             public void ModifyStatArguments(RecalculateStatsAPI.StatHookEventArgs args)
             {
-                args.damageMultAdd += stack;
-                args.attackSpeedMultAdd -= MSUtil.InverseHyperbolicScaling(0.4f, 0.4f, 0.9f, stack);
-                args.cooldownMultAdd += MSUtil.InverseHyperbolicScaling(0.4f, 0.4f, 0.9f, stack);
+                args.damageMultAdd += damageMultiplier;
+                float penalty = MSUtil.InverseHyperbolicScaling(forcePenalty, forcePenalty, 0.9f, stack);
+                args.attackSpeedMultAdd -= penalty;
+                args.cooldownMultAdd += penalty;
+            }
+            public void RecalculateStatsEnd()
+            {
+                if (body.skillLocator)
+                {
+                    float cooldownPenalty = MSUtil.InverseHyperbolicScaling(forcePenalty, forcePenalty, 0.9f, stack);
+                    SS2Log.Debug("Relic Stats - " + cooldownPenalty);
+                    if (body.skillLocator.primary)
+                    {
+                        body.skillLocator.primary.flatCooldownReduction += 0;
+                        body.skillLocator.primary.cooldownScale *= cooldownPenalty;
+                    }
+                    if (body.skillLocator.secondary)
+                    {
+                        body.skillLocator.secondary.flatCooldownReduction += 0;
+                        body.skillLocator.secondary.cooldownScale *= cooldownPenalty;
+                    }
+                    if (body.skillLocator.utility)
+                    {
+                        body.skillLocator.utility.flatCooldownReduction += 0;
+                        body.skillLocator.utility.cooldownScale *= cooldownPenalty;
+                    }
+                    if (body.skillLocator.special)
+                    {
+                        body.skillLocator.special.flatCooldownReduction += 0;
+                        body.skillLocator.special.cooldownScale *= cooldownPenalty;
+                    }
+                        
+                }
+            }
+
+            public void RecalculateStatsStart()
+            {
+                
+            }
+            private static void RecalculateStatsAPI_GetStatCoefficients(CharacterBody self, RecalculateStatsAPI.StatHookEventArgs args)
+            {
             }
         }
     }
