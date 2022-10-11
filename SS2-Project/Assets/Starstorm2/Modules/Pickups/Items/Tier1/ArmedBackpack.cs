073cd0f850e2f383b970e68cc33ec4bb6a2864b8
Lilith <zenithrium@gmail.com>
2022-10-11 00:25:46 -0400

    reworks armed backpack
    it does something now (but could probably be adjusted - not sure i'm satisfied on the proc chance implementation)

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/ArmedBackpack.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/ArmedBackpack.cs
@@ -13,53 +13,46 @@ namespace Moonstorm.Starstorm2.Items
         [TokenModifier("SS2_ITEM_ARMEDBACKPACK_DESC", StatTypes.Percentage, 0)]
         public static float backpackDamage = 1f;
 
-        [ConfigurableField(ConfigDesc = "Proc chance of the backpack. (100 = 100%)")]
+        [ConfigurableField(ConfigDesc = "Proc multiplier per percentage of health lost. (1 = 100% of health fraction lost)")]
         [TokenModifier("SS2_ITEM_ARMEDBACKPACK_DESC", StatTypes.Percentage, 0)]
-        public static float procChance = 15f;
+        public static float procMult = 2f;
 
-        public sealed class Behavior : BaseItemBodyBehavior, IOnDamageDealtServerReceiver
+        public static ProcChainMask ignoredProcs;
+
+        public sealed class Behavior : BaseItemBodyBehavior, IOnTakeDamageServerReceiver
         {
             [ItemDefAssociation]
             private static ItemDef GetItemDef() => SS2Content.Items.ArmedBackpack;
-            public void OnDamageDealtServer(DamageReport report)
+
+            public void OnTakeDamageServer(DamageReport damageReport)
             {
-                if (Util.CheckRoll(procChance, body.master))
+                if(stack > 0 && damageReport.damageDealt > 0)
                 {
-                    if (report.damageInfo.procCoefficient > 0)
+                    float percentHPLoss = (damageReport.damageDealt / damageReport.victim.fullCombinedHealth) * 100f * procMult;
+                    var playerBody = damageReport.victimBody;
+
+                    if (Util.CheckRoll(percentHPLoss, playerBody.master))
                     {
+                        float damageCoefficient = backpackDamage * stack;
+                        float missileDamage = playerBody.damage * damageCoefficient;
 
-                        BullseyeSearch bullseyeSearch = new BullseyeSearch();
-                        //EffectManager.SimpleMuzzleFlash();
-                        TeamComponent team = GetComponent<TeamComponent>();
-                        bullseyeSearch.teamMaskFilter = TeamMask.all;
-                        bullseyeSearch.teamMaskFilter.RemoveTeam(team.teamIndex);
-                        bullseyeSearch.filterByLoS = true;
-                        bullseyeSearch.searchOrigin = body.aimOrigin;
-                        bullseyeSearch.searchDirection = -body.characterDirection.targetVector;
-                        bullseyeSearch.sortMode = BullseyeSearch.SortMode.Angle;
-                        bullseyeSearch.maxDistanceFilter = 1000;
-                        bullseyeSearch.maxAngleFilter = 45f;
-                        bullseyeSearch.RefreshCandidates();
-                        HurtBox target = bullseyeSearch.GetResults().FirstOrDefault<HurtBox>();
-
-                        if (!target)
-                            return;
-
-                        MSUtil.PlayNetworkedSFX("ExecutionerPrimary", gameObject.transform.position);
-                        new BulletAttack
+                        var teamIndex = damageReport.attackerTeamIndex;
+                        var attacker = damageReport.attacker;
+                        
+                        if (teamIndex == TeamIndex.None || teamIndex == TeamIndex.Player )
                         {
-                            owner = gameObject,
-                            weapon = gameObject,
-                            origin = body.aimOrigin,
-                            aimVector = target.transform.position - body.aimOrigin,
-                            minSpread = 0,
-                            maxSpread = 0,
-                            damage = backpackDamage * body.damage,
-                            force = 50,
-                            isCrit = Util.CheckRoll(body.crit),
-                            damageType = DamageType.Generic
-                        }.Fire();
-
+                            attacker = null; //this prevents it from firing into blood shrines and i guess yourself/teammates if that lunar active is involved
+                        }
+                        MissileUtils.FireMissile(
+                            playerBody.corePosition,
+                            playerBody,
+                            ignoredProcs,
+                            attacker, 
+                            missileDamage,
+                            Util.CheckRoll(playerBody.crit, playerBody.master),
+                            GlobalEventManager.CommonAssets.missilePrefab,
+                            DamageColorIndex.Item,
+                            true);
                     }
                 }
             }
