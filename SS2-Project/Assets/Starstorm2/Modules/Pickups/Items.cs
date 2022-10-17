197f2322488b42962862df55e3f8770ce929e0b1
gaforb <38703458+gaforb@users.noreply.github.com>
2022-10-17 21:57:10 +0100

    item enable/disable
    It's literally 2 lines?? There's no way this works, do not merge.

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items.cs
@@ -1,4 +1,5 @@
 ﻿using R2API.ScriptableObjects;
+using RoR2;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,6 +8,7 @@ namespace Moonstorm.Starstorm2.Modules
     public sealed class Items : ItemModuleBase
     {
         public static Items Instance { get; private set; }
+        public static ItemDef[] LoadedSS2Items { get => SS2Content.Instance.SerializableContentPack.itemDefs; }
         public override R2APISerializableContentPack SerializableContentPack => SS2Content.Instance.SerializableContentPack;
 
         public override void Initialize()
@@ -20,6 +22,7 @@ namespace Moonstorm.Starstorm2.Modules
         protected override IEnumerable<ItemBase> GetItemBases()
         {
             base.GetItemBases()
+                .Where(item => SS2Main.config.Bind<bool>(item.ItemDef.name, "Enable Item", true, "Wether or not to enable this item.").Value)
                 .ToList()
                 .ForEach(item => AddItem(item));
             return null;
