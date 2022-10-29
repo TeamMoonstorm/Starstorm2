fe55f9274d0192328debd2c6bef0085fc575be21
Nebby <nebby131999@gmail.com>
2022-10-29 16:30:20 -0300

    This is genuenly fucking retarded

---
+++ SS2-Project/Assets/EditorUtilities/ProjectSpecificImporter/InstallAncientScepter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ThunderKit.Core;
+using ThunderKit.Core.Config;
+using ThunderKit.Integrations.Thunderstore;
+
+namespace Moonstorm.Starstorm2.Editor
+{
+    internal class InstallAncientScepter : ThunderstorePackageInstaller
+    {
+        public override string DependencyId => "amogus_lovers-StandaloneAncientScepter";
+
+        public override string ThunderstoreAddress => "https://thunderstore.io";
+
+        public override int Priority => RiskOfThunder.RoR2Importer.Constants.Priority.InstallMLAPI - 155_500;
+    }
+}
