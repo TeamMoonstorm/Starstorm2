2a206ae90bd12b7320ec14efa78f8a5d0196d99f
Lilith <zenithrium@gmail.com>
2022-11-15 18:36:34 -0500

    x4 stuff
    needs cleanup

---
+++ SS2-Project/Assets/Starstorm2/Modules/Buffs/BuffTypes/X4Buff.cs
@@ -0,0 +1,36 @@
+﻿using Moonstorm.Components;
+using R2API;
+using RoR2;
+using System;
+
+namespace Moonstorm.Starstorm2.Buffs
+{
+    public sealed class X4Buff : BuffBase
+    {
+        public override BuffDef BuffDef { get; } = SS2Assets.LoadAsset<BuffDef>("BuffX4");
+
+        public sealed class X4BuffBehavior : BaseBuffBodyBehavior, IBodyStatArgModifier
+        {
+            [BuffDefAssociation]
+
+            private static BuffDef GetBuffDef() => SS2Content.Buffs.BuffX4;
+
+            //private float atkMult = Items.X4.atkSpeedBonus;
+
+            public void ModifyStatArguments(RecalculateStatsAPI.StatHookEventArgs args)
+            {
+                //args.attackSpeedMultAdd += atkMult;
+                if (body.HasBuff(SS2Content.Buffs.BuffX4))
+                {
+                    args.baseRegenAdd += (Items.X4.baseRegenBoost + ((Items.X4.baseRegenBoost / 5) * body.level)) * buffStacks; //original code not taken from bitter root becasue i was lazy
+                    
+                    //args.regenMultAdd += Items.X4.extraRegeneration;
+
+
+                    //args.baseAttackSpeedAdd += Items.X4.baseRegenBoost * buffStacks;
+                }
+                //throw new NotImplementedException();
+            }
+        }
+    }
+}
