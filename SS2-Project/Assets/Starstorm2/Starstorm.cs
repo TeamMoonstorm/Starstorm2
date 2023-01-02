f38872641ddf816f3abf91eeb3443de18d442739
Lilith <zenithrium@gmail.com>
2023-01-01 22:58:14 -0500

    thunderkit is ignoring my nemmando change

--- SS2-Project/Assets/Starstorm2/Starstorm.cs
+++ SS2-Project/Assets/Starstorm2/Starstorm.cs
@@ -27,7 +27,7 @@ namespace Moonstorm.Starstorm2
     {
         internal const string guid = "com.TeamMoonstorm.Starstorm2-Nightly";
         internal const string modName = "Starstorm 2 Nightly";
-        internal const string version = "0.3.47";
+        internal const string version = "0.3.49";
 
         public static Starstorm instance;
         public static PluginInfo pluginInfo;
