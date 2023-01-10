212f1a7991e528ef83241c69aa7d88282c6be200
swuff-star <swuff-star@users.noreply.github.com>
2023-01-10 16:18:54 -0500

    oh boy

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/MoltenCoin.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/MoltenCoin.cs
@@ -28,13 +28,14 @@ namespace Moonstorm.Starstorm2.Items
                             victimObject = report.victim.gameObject,
                             dotIndex = DotController.DotIndex.Burn,
                             duration = report.damageInfo.procCoefficient * 4f,
-                            damageMultiplier = 1f + (stack * 1f)
-                            //If you're trying to configure this and are so desperate you've come here, I don't have a damn clue.
+                            damageMultiplier = stack
                         };
                         StrengthenBurnUtils.CheckDotForUpgrade(report.attackerBody.inventory, ref dotInfo);
                         DotController.InflictDot(ref dotInfo);
+
                         body.master.GiveMoney((uint)(stack * (Run.instance.stageClearCount + (1 * 1f))));
                         MSUtil.PlayNetworkedSFX("MoltenCoin", report.victim.gameObject.transform.position);
+                        EffectManager.SimpleImpactEffect(HealthComponent.AssetReferences.gainCoinsImpactEffectPrefab, body.transform.position, UnityEngine.Vector3.up, true);
                     }
                 }
             }
