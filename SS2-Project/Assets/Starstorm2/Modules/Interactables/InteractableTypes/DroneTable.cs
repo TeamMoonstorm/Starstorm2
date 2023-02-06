1b33d456b41897dc8af6e7bef7575531af492b91
Lilith <zenithrium@gmail.com>
2023-02-06 01:42:42 -0500

    Why the Fuck

--- SS2-Project/Assets/Starstorm2/Modules/Interactables/InteractableTypes/DroneTable.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Interactables/InteractableTypes/DroneTable.cs
@@ -15,95 +15,95 @@ namespace Moonstorm.Starstorm2.Interactables
 
         private static GameObject interactable;
 
-        public override MSInteractableDirectorCard InteractableDirectorCard { get; } = SS2Assets.LoadAsset<MSInteractableDirectorCard>("midcDropPod");
+        public override MSInteractableDirectorCard InteractableDirectorCard { get; } = SS2Assets.LoadAsset<MSInteractableDirectorCard>("midcDroneTable");
 
-        public override void Initialize()
-        {
-            //DirectorAPI.MonsterActions += HandleAvailableMonsters;
-
-            var survivorPod = Resources.Load<GameObject>("prefabs/networkedobjects/SurvivorPod");
-            interactable = PrefabAPI.InstantiateClone(survivorPod, "DropPod", false);
-
-            Interactable.transform.position = Vector3.zero;
-
-            DestroyUneededObjectsAndComponents();
-            EnableObjects();
-            ModifyExistingComponents();
-            AddNewComponents();
-
-            HG.ArrayUtils.ArrayAppend(ref SS2Content.Instance.SerializableContentPack.networkedObjectPrefabs, Interactable);
-            InteractableDirectorCard.prefab = Interactable;
-        }
-
-        private void HandleAvailableMonsters(System.Collections.Generic.List<DirectorAPI.DirectorCardHolder> arg1, DirectorAPI.StageInfo arg2)
-        {
-            DropPodController.currentStageMonsters = arg1.Where(cardHolder => cardHolder.MonsterCategory == DirectorAPI.MonsterCategory.BasicMonsters)
-                                                         .Select(cardHolder => cardHolder.Card)
-                                                         .ToArray();
-        }
-
-        private void DestroyUneededObjectsAndComponents()
-        {
-            Object.Destroy(Interactable.GetComponent<SurvivorPodController>());
-            Object.Destroy(Interactable.GetComponent<VehicleSeat>());
-            //Interactable.GetComponents<InstantiatePrefabOnStart>().ToList().ForEach(component => Object.Destroy(component));
-            Object.Destroy(Interactable.GetComponent<BuffPassengerWhileSeated>());
-
-            var flames = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base/Flames");
-            Object.Destroy(flames.gameObject);
-
-            var donut = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base/FireDonut");
-            Object.Destroy(donut.gameObject);
-
-            /*var mdl = Interactable.transform.Find("Base/mdlEscapePod");
-            Object.Destroy(mdl.GetComponent<Animator>());*/
-        }
-
-        private void EnableObjects()
-        {
-            var debris = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base/DebrisParent");
-            debris.gameObject.SetActive(true);
-        }
-
-        private void ModifyExistingComponents()
-        {
-            var stateMachine = Interactable.GetComponent<EntityStateMachine>();
-            stateMachine.initialStateType = new SerializableEntityStateType(typeof(EntityStates.DropPod.Idle));
-            stateMachine.mainStateType = new SerializableEntityStateType(typeof(Idle));
-
-            var modelLocator = Interactable.GetComponent<ModelLocator>();
-            modelLocator.enabled = true;
-            modelLocator.modelBaseTransform = null;
-
-            var exitPos = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/ExitPosition");
-            exitPos.transform.localPosition = new Vector3(0, -2, 0);
-            var cLocator = modelLocator.modelTransform.GetComponent<ChildLocator>();
-            HG.ArrayUtils.ArrayAppend(ref cLocator.transformPairs, new ChildLocator.NameTransformPair { name = "ExitPos", transform = exitPos });
-        }
-
-        private void AddNewComponents()
-        {
-            var networkTransform = Interactable.AddComponent<NetworkTransform>();
-            networkTransform.transformSyncMode = NetworkTransform.TransformSyncMode.SyncTransform;
-            Interactable.AddComponent<GenericDisplayNameProvider>().displayToken = "SS2_INTERACTABLE_DROPPOD_NAME";
-            Interactable.AddComponent<DropPodController>();
-
-            PaintDetailsBelow details = Interactable.AddComponent<PaintDetailsBelow>();
-            details.influenceOuter = 2;
-            details.influenceInner = 1;
-            details.layer = 0;
-            details.density = 0.5f;
-            details.densityPower = 3;
-
-            details = Interactable.AddComponent<PaintDetailsBelow>();
-            details.influenceOuter = 2;
-            details.influenceInner = 1;
-            details.layer = 1;
-            details.density = 0.3f;
-            details.densityPower = 3;
-
-            var podMesh = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base").gameObject;
-            podMesh.AddComponent<EntityLocator>().entity = Interactable;
-        }
+        //public override void Initialize()
+        //{
+        //    //DirectorAPI.MonsterActions += HandleAvailableMonsters;
+        //
+        //    var survivorPod = Resources.Load<GameObject>("prefabs/networkedobjects/SurvivorPod");
+        //    interactable = PrefabAPI.InstantiateClone(survivorPod, "DropPod", false);
+        //
+        //    Interactable.transform.position = Vector3.zero;
+        //
+        //    DestroyUneededObjectsAndComponents();
+        //    EnableObjects();
+        //    ModifyExistingComponents();
+        //    AddNewComponents();
+        //
+        //    HG.ArrayUtils.ArrayAppend(ref SS2Content.Instance.SerializableContentPack.networkedObjectPrefabs, Interactable);
+        //    InteractableDirectorCard.prefab = Interactable;
+        //}
+        //
+        //private void HandleAvailableMonsters(System.Collections.Generic.List<DirectorAPI.DirectorCardHolder> arg1, DirectorAPI.StageInfo arg2)
+        //{
+        //    DropPodController.currentStageMonsters = arg1.Where(cardHolder => cardHolder.MonsterCategory == DirectorAPI.MonsterCategory.BasicMonsters)
+        //                                                 .Select(cardHolder => cardHolder.Card)
+        //                                                 .ToArray();
+        //}
+        //
+        //private void DestroyUneededObjectsAndComponents()
+        //{
+        //    Object.Destroy(Interactable.GetComponent<SurvivorPodController>());
+        //    Object.Destroy(Interactable.GetComponent<VehicleSeat>());
+        //    //Interactable.GetComponents<InstantiatePrefabOnStart>().ToList().ForEach(component => Object.Destroy(component));
+        //    Object.Destroy(Interactable.GetComponent<BuffPassengerWhileSeated>());
+        //
+        //    var flames = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base/Flames");
+        //    Object.Destroy(flames.gameObject);
+        //
+        //    var donut = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base/FireDonut");
+        //    Object.Destroy(donut.gameObject);
+        //
+        //    /*var mdl = Interactable.transform.Find("Base/mdlEscapePod");
+        //    Object.Destroy(mdl.GetComponent<Animator>());*/
+        //}
+        //
+        //private void EnableObjects()
+        //{
+        //    var debris = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base/DebrisParent");
+        //    debris.gameObject.SetActive(true);
+        //}
+        //
+        //private void ModifyExistingComponents()
+        //{
+        //    var stateMachine = Interactable.GetComponent<EntityStateMachine>();
+        //    stateMachine.initialStateType = new SerializableEntityStateType(typeof(EntityStates.DropPod.Idle));
+        //    stateMachine.mainStateType = new SerializableEntityStateType(typeof(Idle));
+        //
+        //    var modelLocator = Interactable.GetComponent<ModelLocator>();
+        //    modelLocator.enabled = true;
+        //    modelLocator.modelBaseTransform = null;
+        //
+        //    var exitPos = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/ExitPosition");
+        //    exitPos.transform.localPosition = new Vector3(0, -2, 0);
+        //    var cLocator = modelLocator.modelTransform.GetComponent<ChildLocator>();
+        //    HG.ArrayUtils.ArrayAppend(ref cLocator.transformPairs, new ChildLocator.NameTransformPair { name = "ExitPos", transform = exitPos });
+        //}
+        //
+        //private void AddNewComponents()
+        //{
+        //    var networkTransform = Interactable.AddComponent<NetworkTransform>();
+        //    networkTransform.transformSyncMode = NetworkTransform.TransformSyncMode.SyncTransform;
+        //    Interactable.AddComponent<GenericDisplayNameProvider>().displayToken = "SS2_INTERACTABLE_DROPPOD_NAME";
+        //    Interactable.AddComponent<DropPodController>();
+        //
+        //    PaintDetailsBelow details = Interactable.AddComponent<PaintDetailsBelow>();
+        //    details.influenceOuter = 2;
+        //    details.influenceInner = 1;
+        //    details.layer = 0;
+        //    details.density = 0.5f;
+        //    details.densityPower = 3;
+        //
+        //    details = Interactable.AddComponent<PaintDetailsBelow>();
+        //    details.influenceOuter = 2;
+        //    details.influenceInner = 1;
+        //    details.layer = 1;
+        //    details.density = 0.3f;
+        //    details.densityPower = 3;
+        //
+        //    var podMesh = Interactable.transform.Find("Base/mdlEscapePod/EscapePodArmature/Base").gameObject;
+        //    podMesh.AddComponent<EntityLocator>().entity = Interactable;
+        //}
     }
 }
