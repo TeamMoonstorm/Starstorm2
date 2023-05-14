b2d2a7e5ae19cfb7b4f27218db3b2e308d01a491
Lilith <zenithrium@gmail.com>
2023-05-13 20:25:58 -0400

    needles works again lol

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/Needles.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/Needles.cs
@@ -55,24 +55,28 @@ namespace Moonstorm.Starstorm2.Items
                 if (damageInfo.attacker)
                 {
                     CharacterBody attackerBody = damageInfo.attacker.GetComponent<CharacterBody>();
-                    if (attackerBody && !damageInfo.rejected && NetworkServer.active)
+                    if (attackerBody && !damageInfo.rejected && NetworkServer.active) // && damageInfo.procCoefficient > 0f) //then dots can't apply stacks
                     {
+                        //SS2Log.Info("original if");
                         //doNeedleProc(self); 
-                    }
-                    else
-                    {
+                    //}
+                    //else //what the fuck is this else for again?
+                    //{
+                        //SS2Log.Info("original else");
                         //P(A+B) = P(A) + P(B) - P(AB)
                         //float intendedChance = attackerBody.crit + (self.body.GetBuffCount(SS2Content.Buffs.BuffNeedleBuildup) * attackerBody.inventory.GetItemCount(SS2Content.Items.Needles)); //assuming each buff is 1% per items
                         float intendedChance = attackerBody.crit + (self.body.GetBuffCount(SS2Content.Buffs.BuffNeedleBuildup) * bonusCrit);
                         float secondChance = (attackerBody.crit - intendedChance) / (attackerBody.crit - 100) * 100;
                         bool secondCrit = Util.CheckRoll(secondChance);
                         damageInfo.crit = secondCrit;
-                        if (damageInfo.crit && damageInfo.procCoefficient > 0f)
+                        if (damageInfo.crit && damageInfo.procCoefficient > 0f) //&& damageInfo.damageType != DamageType.DoT
                         {
-                            //doNeedleProc(self); 
+                            //SS2Log.Info("second if");
+                            doNeedleProc(self); 
                         }
                         else
                         {
+                            //SS2Log.Info("second else");
                             //P(A+B) = P(A) + P(B) - P(AB)
                             //intendedChance = attackerBody.crit + (self.body.GetBuffCount(SS2Content.Buffs.BuffNeedleBuildup) * attackerBody.inventory.GetItemCount(SS2Content.Items.Needles)); //assuming each buff is 1% per items
                             intendedChance = attackerBody.crit + (self.body.GetBuffCount(SS2Content.Buffs.BuffNeedleBuildup) * bonusCrit);
@@ -80,12 +84,14 @@ namespace Moonstorm.Starstorm2.Items
                             secondChance = (attackerBody.crit - intendedChance) / (attackerBody.crit - 100) * 100;
                             secondCrit = Util.CheckRoll(secondChance);
                             damageInfo.crit = secondCrit;
-                            if (damageInfo.crit && damageInfo.procCoefficient > 0f)
+                            if (damageInfo.crit && damageInfo.procCoefficient > 0f) //&& damageInfo.damageType != DamageType.DoT
                             {
+                                //SS2Log.Info("third if");
                                 doNeedleProc(self);
                             }
                             else
                             {
+                                //SS2Log.Info("third else");
                                 var tracker = self.body.gameObject.GetComponent<NeedleTracker>();
                                 if (!tracker)
                                 {
