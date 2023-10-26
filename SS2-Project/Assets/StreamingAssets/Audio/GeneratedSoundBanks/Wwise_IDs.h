65fa0ca41d4c51b66c63ea4caae69efd54ac1f6b
Lilith <zenithrium@gmail.com>
2023-10-26 18:55:31 -0400

    ggraahh2

--- SS2-Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ SS2-Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -169,15 +169,3 @@ namespace AK
 }// namespace AK
 
 #endif // __WWISE_IDS_H__
-3803692087U;
-    } // namespace BUSSES
-
-    namespace AUDIO_DEVICES
-    {
-        static const AkUniqueID NO_OUTPUT = 2317455096U;
-        static const AkUniqueID SYSTEM = 3859886410U;
-    } // namespace AUDIO_DEVICES
-
-}// namespace AK
-
-#endif // __WWISE_IDS_H__
