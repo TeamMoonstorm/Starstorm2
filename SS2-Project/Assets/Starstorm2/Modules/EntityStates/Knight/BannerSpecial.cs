e78cdbb278883dc10c90dd0a6709da5ab3d353b2
hevia <anthony@hevia.dev>
2023-11-13 23:37:52 -0800

    All skills in the right place yippie

--- SS2-Project/Assets/Starstorm2/Modules/EntityStates/Knight/BannerSpecial.cs
+++ SS2-Project/Assets/Starstorm2/Modules/EntityStates/Knight/BannerSpecial.cs
@@ -1,13 +1,58 @@
 ﻿using EntityStates;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static Moonstorm.Starstorm2.Equipments.GreaterWarbanner;
+using UnityEngine.Networking;
+using RoR2;
+using RoR2.Audio;
+using Moonstorm.Starstorm2;
 
+namespace EntityStates.Knight
+{
 public class BannerSpecial : BaseState
 {
+
+    public EquipmentDef EquipmentDef { get; } = SS2Assets.LoadAsset<EquipmentDef>("GreaterWarbanner", SS2Bundle.Equipments);
+    public GameObject WarbannerObject { get; set; } = SS2Assets.LoadAsset<GameObject>("GreaterWarbannerWard", SS2Bundle.Equipments);
     public override void OnEnter()
     {
-        base.OnEnter();
+        var GBToken = characterBody.gameObject.GetComponent<GreaterBannerToken>();
+        if (!GBToken)
+        {
+            characterBody.gameObject.AddComponent<GreaterBannerToken>();
+            GBToken = characterBody.gameObject.GetComponent<GreaterBannerToken>();
+        }
+
+        Vector3 position = inputBank.aimOrigin - (inputBank.aimDirection);
+        GameObject bannerObject = UnityEngine.Object.Instantiate(WarbannerObject, position, Quaternion.identity);
+
+        bannerObject.GetComponent<TeamFilter>().teamIndex = teamComponent.teamIndex;
+        NetworkServer.Spawn(bannerObject);
+
+
+        if (GBToken.soundCooldown >= 5f)
+        {
+            var sound = NetworkSoundEventCatalog.FindNetworkSoundEventIndex("GreaterWarbanner");
+            EffectManager.SimpleSoundEffect(sound, bannerObject.transform.position, true);
+            GBToken.soundCooldown = 0f;
+        }
+
+        GBToken.ownedBanners.Add(bannerObject);
+
+        if (GBToken.ownedBanners.Count > maxGreaterBanners)
+        {
+            //SS2Log.Debug("Removing oldest Warbanner");
+            var oldBanner = GBToken.ownedBanners[0];
+            GBToken.ownedBanners.RemoveAt(0);
+            EffectData effectData = new EffectData
+            {
+                origin = oldBanner.transform.position
+            };
+            effectData.SetNetworkedObjectReference(oldBanner);
+            EffectManager.SpawnEffect(HealthComponent.AssetReferences.executeEffectPrefab, effectData, transmit: true);
+
+            UnityEngine.Object.Destroy(oldBanner);
+            NetworkServer.Destroy(oldBanner);
+        }
     }
 
     public override void FixedUpdate()
@@ -24,4 +69,6 @@ public class BannerSpecial : BaseState
     {
         return InterruptPriority.PrioritySkill;
     }
+}
+
 }
