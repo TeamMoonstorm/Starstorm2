952b02054c461fd825d84f22b03337fba4b7d1e3
swuff-star <63352264+swuff-star@users.noreply.github.com>
2023-12-27 02:01:40 -0500

    lamp changes part 1
    + redid wayfarer projectiles
    + hopefully fixed an issue where lamp enemies dont die correctly
    + redid follower ai to be more combative
    + nerfed wayfarer health
    + updated most recent full moon
    + started void rock interactable

--- SS2-Project/Assets/Starstorm2/Modules/Interactables/InteractableTypes/VoidRock.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Interactables/InteractableTypes/VoidRock.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShrineVoidRockBehavior : MonoBehaviour
+public class VoidRock : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
