7e44a2edec3c608fd60bd6724520cf8bce028c07
swuff-star <63352264+swuff-star@users.noreply.github.com>
2024-01-15 23:28:25 -0500

    IDK LOL
    GWAAHHAHA
    STARTED COMBAT DIRECTOR (BUT NOT REALLY)
    field accelerator change???
    ...????

---
+++ SS2-Project/Assets/Starstorm2/Modules/RunBehaviors/StarstormBehavior.cs
@@ -0,0 +1,60 @@
+﻿using R2API;
+using R2API.Utils;
+using RoR2;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+namespace Moonstorm.Starstorm2.Components
+{
+    public class StarstormBehavior : MonoBehaviour
+    {
+        public static StarstormBehavior instance { get; private set; }
+        public Run run;
+
+        internal static void Init()
+        {
+ 
+        }
+
+        private void Awake()
+        {
+            run = GetComponentInParent<Run>();
+        }
+
+        private void Start()
+        {
+            instance = this;
+            On.RoR2.SceneDirector.Start += SceneDirector_Start;
+            On.RoR2.CombatDirector.Awake += CombatDirector_Awake;
+        }
+
+
+        private void OnDestroy()
+        {
+            On.RoR2.SceneDirector.Start -= SceneDirector_Start;
+        }
+
+        public static void SceneDirector_Start(On.RoR2.SceneDirector.orig_Start orig, SceneDirector self)
+        {
+            orig(self);
+        }
+
+        public static void CombatDirector_Awake(On.RoR2.CombatDirector.orig_Awake orig, CombatDirector self)
+        {
+            orig(self);
+            Debug.Log("I'M WAKING UP");
+            Debug.Log("TO ASH AND DUST");
+            Debug.Log("CHECK THIS SHIT OUT:");
+            Debug.Log(self.eliteBias + " : ELITE BIAS");
+            Debug.Log(self.consecutiveCheapSkips + " : CONSEC CHEAP SKIPS");
+            Debug.Log(self.moneyWaves + " : MONEY WAVES");
+            Debug.Log(self.moneyWaveIntervals + " : MONEY WAVE INTERVALS");
+            Debug.Log(self.creditMultiplier + " : CREDIT MULTIPLIER");
+            Debug.Log("KTHXBAI");
+        }
+    }
+}
