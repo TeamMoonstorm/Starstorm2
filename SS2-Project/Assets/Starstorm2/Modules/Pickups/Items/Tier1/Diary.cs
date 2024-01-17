3a69b6bbc8c71589404dc044e93afb8303ed2009
nayDPzz <88465294+nayDPz@users.noreply.github.com>
2024-01-17 16:24:39 -0500

    adjust volume + removed debuff on chirr special

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/Diary.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/Diary.cs
@@ -47,92 +47,19 @@ namespace Moonstorm.Starstorm2.Items
             }
             [ItemDefAssociation(useOnClient = false, useOnServer = true)]
             private static ItemDef GetItemDef() => SS2Content.Items.Diary;
-            private bool expectBossKill = false;
-            private float localTime;
-            private List<TimedExpOffset> pendingOffsets = new List<TimedExpOffset>();
-            private float nextAward;
-            private void AddPendingOffset(float awardTime, long awardAmount)
-            {
-                this.pendingOffsets.Add(new TimedExpOffset
-                {
-                    awardTime = awardTime,
-                    offset = awardAmount
-                });
-                if (this.nextAward > awardTime)
-                {
-                    this.nextAward = awardTime;
-                }
-            }
-            private void FixedUpdate()
-            {
-                this.localTime += Time.fixedDeltaTime;
-                if (this.pendingOffsets.Count > 0 && this.nextAward <= this.localTime)
-                {
-                    this.nextAward = float.PositiveInfinity;
-                    for (int i = this.pendingOffsets.Count - 1; i >= 0; i--)
-                    {
-                        TimedExpOffset timedExpOffset = pendingOffsets[i];
-                        if (timedExpOffset.awardTime <= this.localTime)
-                        {
-                            master.SS2OffsetExperience(timedExpOffset.offset, true);
-                            this.pendingOffsets.RemoveAt(i);
-                            if (master.hasBody)
-                            {
-                                GameObject bodyObject = master.GetBodyObject();
-                                PlayDiarySFXLocal(bodyObject);
-                                NetworkIdentity networkIdentity = bodyObject.GetComponent<NetworkIdentity>();
-                                if (networkIdentity)
-                                {
-                                    //we need to give clients an opportunity to play the diary sfx
-                                    new DiarySFXMessage(networkIdentity.netId).Send(NetworkDestination.Clients);
-                                }
-
-                            }
-                        }
-                        else if (timedExpOffset.awardTime < this.nextAward)
-                        {
-                            this.nextAward = timedExpOffset.awardTime;
-                        }
-                    }
-                }
-            }
             private void OnEnable()
             {
-                On.RoR2.ExperienceManager.AwardExperience += ExperienceManager_AwardExperience;
-                On.RoR2.DeathRewards.OnKilledServer += DeathRewards_OnKilledServer;
+                Stage.onStageStartGlobal += AddLevel;
             }
 
-            private void ExperienceManager_AwardExperience(On.RoR2.ExperienceManager.orig_AwardExperience orig, ExperienceManager self, Vector3 origin, CharacterBody body, ulong amount)
+            private void AddLevel(Stage stage)
             {
-                orig(self, origin, body, amount);
-                if (expectBossKill)
-                {
-                    List<ulong> list = self.CalculateDenominations(amount);
-                    uint num = 0U;
-                    for (int i = 0; i < list.Count; i++)
-                    {
-                        this.AddPendingOffset(this.localTime + 0.5f + 1.5f * ExperienceManager.orbTimeOffsetSequence[(int)num], (long)Mathf.CeilToInt(list[i] * experienceBonus * stack));
-                        num += 1U;
-                        if ((ulong)num >= (ulong)((long)ExperienceManager.orbTimeOffsetSequence.Length))
-                        {
-                            num = 0U;
-                        }
-                    }
-                    
-                }
-            }
-
-            private void DeathRewards_OnKilledServer(On.RoR2.DeathRewards.orig_OnKilledServer orig, DeathRewards self, DamageReport damageReport)
-            {
-                expectBossKill = self.characterBody && self.characterBody.isBoss && damageReport.attackerTeamIndex == master.teamIndex;
-                orig(self, damageReport);
-                expectBossKill = false;
+                
             }
 
             private void OnDisable()
             {
-                On.RoR2.ExperienceManager.AwardExperience -= ExperienceManager_AwardExperience;
-                On.RoR2.DeathRewards.OnKilledServer -= DeathRewards_OnKilledServer;
+
             }
             private struct TimedExpOffset
             {
@@ -178,51 +105,3 @@ namespace Moonstorm.Starstorm2.Items
         }
     }
 }
-/*public sealed class Behavior : BaseItemBodyBehavior, IStatItemBehavior
-{
-    [ItemDefAssociation]
-    private static ItemDef GetItemDef() => SS2Content.Items.Diary;
-    private float stopwatch;
-
-    public void FixedUpdate()
-    {
-        if (!NetworkServer.active)
-            return;
-        if (Run.instance && !Run.instance.isRunStopwatchPaused)
-        {
-            if (body.master.playerCharacterMasterController)
-            {
-                stopwatch += Time.fixedDeltaTime;
-                if (stopwatch >= 2f)
-                {
-                    if (body.teamComponent)
-                    {
-                        uint exp = (uint)(stack * Math.Pow(2, 1 + (TeamManager.instance.GetTeamLevel(body.teamComponent.teamIndex) / 3.75d)));
-                        TeamManager.instance.GiveTeamExperience(TeamIndex.Player, exp);
-                    }
-
-                    stopwatch = 0f;
-                }
-            }
-        }
-    }
-
-    public void RecalculateStatsStart()
-    {
-        if (NetworkServer.active)
-        {
-            float level = TeamManager.instance.GetTeamLevel(body.teamComponent.teamIndex);
-            if (level > body.level && body.hasEffectiveAuthority)
-            {
-                //N - Kevin works in misterious ways i guess, idk man
-                if (Util.CheckRoll(50)) //G - What the fuck is this doing in recalculatestats
-                { //★ this is fucking stupid yeah
-                    MSUtil.PlayNetworkedSFX("DiaryLevelUp", body.corePosition);
-                }
-            }
-        }
-    }
-
-    public void RecalculateStatsEnd()
-    { }
-}*/
