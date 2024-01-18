6f6328c11283d4b5fac24ae64c63663ce8597233
nayDPzz <88465294+nayDPz@users.noreply.github.com>
2024-01-17 23:25:11 -0500

    trematode increase threshold per stack

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/DetritiveTrematode.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier1/DetritiveTrematode.cs
@@ -1,6 +1,7 @@
 ﻿using Moonstorm.Starstorm2.Buffs;
 using RoR2;
 using RoR2.Items;
+using UnityEngine;
 
 namespace Moonstorm.Starstorm2.Items
 {
@@ -13,18 +14,25 @@ namespace Moonstorm.Starstorm2.Items
         [TokenModifier(token, StatTypes.MultiplyByN, 0, "100")]
         public static float missingHealthPercentage = 0.25f;
 
-        [RooConfigurableField(SS2Config.IDItem, ConfigDesc = "Damage dealt by the Trematode debuff, per second. (1 = 100%)")]
+        [RooConfigurableField(SS2Config.IDItem, ConfigName = "Trematode Threshold Per Stack", ConfigDesc = "Increase in missing health threshold, per stack. (1 = 100%)")]
         [TokenModifier(token, StatTypes.MultiplyByN, 1, "100")]
+        public static float missingHealthPercentagePerStack = 0.1f;
+
+        [RooConfigurableField(SS2Config.IDItem, ConfigDesc = "Damage dealt by the Trematode debuff, per second. (1 = 100%)")]
+        [TokenModifier(token, StatTypes.MultiplyByN, 2, "100")]
         public static float trematodeDamage = 1f;
 
         [RooConfigurableField(SS2Config.IDItem, ConfigDesc = "Movement speed reduction received from the Trematode debuff. (1 = 100%)")]
-        [TokenModifier(token, StatTypes.MultiplyByN, 2, "100")]
+        [TokenModifier(token, StatTypes.MultiplyByN, 3, "100")]
         public static float trematodeSlow = 0.2f;
 
         public sealed class Behavior : BaseItemBodyBehavior, IOnDamageDealtServerReceiver
         {
             [ItemDefAssociation]
             private static ItemDef GetItemDef() => SS2Content.Items.DetritiveTrematode;
+
+            private static BodyIndex fucker = BodyIndex.None;
+            private static BodyIndex Fucker => fucker == BodyIndex.None ? BodyCatalog.FindBodyIndexCaseInsensitive("ArtifactShellBody") : fucker;
             public void OnDamageDealtServer(DamageReport report)
             {
                 var victim = report.victim;
@@ -34,17 +42,27 @@ namespace Moonstorm.Starstorm2.Items
                 if (dotController)
                     hasDot = dotController.HasDotActive(Trematodes.index);
 
-                if (victim.body.bodyIndex == BodyCatalog.FindBodyIndexCaseInsensitive("ArtifactShellBody"))
+                if (victim.body.bodyIndex == Fucker)
                     return;
 
-                if (victim.combinedHealthFraction < missingHealthPercentage && !hasDot && (victim.gameObject != attacker))
+                //25% + 10% per stack hyperbolically
+                //1 = 25%
+                //5 = 50.7%
+                //10 = 70.9%
+                //float requiredHealthPercentage = 1 - (1 - missingHealthPercentage) * Mathf.Pow(1 - missingHealthPercentagePerStack, stack - 1);
+
+                //25% + 10% per stack
+                //1 = 25%
+                //5 = 65%
+                // 9 = 105%
+                float requiredHealthPercentage = missingHealthPercentage + missingHealthPercentagePerStack * stack;
+                if (victim.combinedHealthFraction < requiredHealthPercentage && !hasDot && (victim.gameObject != attacker))
                 {
                     var dotInfo = new InflictDotInfo()
                     {
                         attackerObject = attacker,
                         victimObject = victim.gameObject,
                         dotIndex = Trematodes.index,
-                        //duration = report.damageInfo.procCoefficient * dotDuration,
                         duration = -1f,
                         damageMultiplier = trematodeDamage * stack,
                     };
