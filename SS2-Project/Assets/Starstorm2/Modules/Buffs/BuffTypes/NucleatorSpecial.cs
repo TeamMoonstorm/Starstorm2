026a148f7d748dfb02b6613521b113b98bce162a
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2024-05-02 22:21:08 -0400

    Echelon

--- SS2-Project/Assets/Starstorm2/Modules/Buffs/BuffTypes/NucleatorSpecial.cs
+++
@@ -1,8 +0,0 @@
-﻿using RoR2;
-namespace SS2.Buffs
-{
-    /*public sealed class NucleatorSpecial : BuffBase
-    {
-        public override BuffDef BuffDef { get; } = SS2Assets.LoadAsset<BuffDef>("BuffNucleatorSpecial", SS2Bundle.Indev);
-    }*/
-}
