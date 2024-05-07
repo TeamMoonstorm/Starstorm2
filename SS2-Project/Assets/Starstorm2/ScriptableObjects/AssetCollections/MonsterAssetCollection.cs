a5c4853a97fe36198327579ccda2003c3fb12368
nayDPzz <88465294+nayDPz@users.noreply.github.com>
2024-05-07 12:48:17 -0400

    fixed assetcollections
    sory jace

---
+++ SS2-Project/Assets/Starstorm2/ScriptableObjects/AssetCollections/MonsterAssetCollection.cs
@@ -0,0 +1,13 @@
+﻿using RoR2;
+using UnityEngine;
+using MSU;
+using System.Collections.Generic;
+namespace SS2
+{
+    [CreateAssetMenu(fileName = "MonsterAssetCollection", menuName = "Starstorm2/AssetCollections/MonsterAssetCollection")]
+    public class MonsterAssetCollection : BodyAssetCollection
+    {
+        public MonsterCardProvider monsterCardProvider;
+        public R2API.DirectorAPI.DirectorCardHolder dissonanceCardHolder;
+    }
+}
---
+++ SS2-Project/Assets/Starstorm2/ScriptableObjects/AssetCollections/SurvivorAssetCollection.cs
@@ -0,0 +1,11 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace SS2
+{
+    [CreateAssetMenu(fileName = "SurvivorAssetCollection", menuName = "Starstorm2/AssetCollections/SurvivorAssetCollection")]
+    public class SurvivorAssetCollection : BodyAssetCollection
+    {
+        public SurvivorDef survivorDef;
+    }
+}
