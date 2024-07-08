376e0c0332221884342196ab5e5335fc9ba02165
hevia <anthony@hevia.dev>
2024-07-08 12:52:40 -0700

    balloon implemented

--- SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier2/Balloon.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Pickups/Items/Tier2/Balloon.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using MSU;
+using R2API;
+using RoR2;
+using RoR2.ContentManagement;
+using RoR2.Items;
+using SS2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.Networking;
+using UnityEngine;
+using MSU.Config;
+using static AkMIDIEvent;
 
 namespace Assets.Starstorm2.Modules.Pickups.Items.Tier2
 {
-    internal class Balloon
+    public class Balloon : SS2Item
     {
+        public override SS2AssetRequest AssetRequest => throw new NotImplementedException();
+
+        private const string TOKEN = "LIT_ITEM_RUSTYJETPACK_DESC";
+
+        //[RiskOfOptionsConfigureField(LITConfig.ITEMS, ConfigDescOverride = "Amount of gravity removed, as a pecent")]
+        //[FormatToken(TOKEN, FormatTokenAttribute.OperationTypeEnum.MultiplyByN, 100, 2)]
+        public static float reducedGravity = 0.30f;
+
+        public override void Initialize()
+        {
+        }
+
+        public class BalloonBehavior : BaseItemBodyBehavior
+        {
+            //[ItemDefAssociation(useOnServer = true, useOnClient = true)]
+           // public static ItemDef GetItemDef() => LITContent.Items.RustyJetpack;
+  
+            private void FixedUpdate()
+            {
+                if (!body.characterMotor || !body)
+                    return;
+
+                if (body.characterMotor.isGrounded)
+                {
+                    return;
+                }
+
+                if (body.inputBank.jump.down)
+                {
+                    body.characterMotor.velocity.y -= Time.fixedDeltaTime * Physics.gravity.y * reducedGravity;
+
+                }
+            }
+        }
+
+        public override bool IsAvailable(ContentPack contentPack)
+        {
+            return false;
+        }
     }
 }
