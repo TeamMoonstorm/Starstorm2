361ae849e146ed141641ebbaf7e4d7fbab4d899a
swuff-star <63352264+swuff-star@users.noreply.github.com>
2024-08-04 20:20:02 -0400

    ummm :3
    added mimic body to game

---
+++ SS2-Project/Assets/Starstorm2/Modules/Characters/Monsters/Mimic.cs
@@ -0,0 +1,25 @@
+﻿using MSU;
+using R2API;
+using RoR2.ContentManagement;
+using System.Collections;
+using UnityEngine;
+namespace SS2.Monsters
+{
+    public sealed class Mimic : SS2Monster
+    {
+        public override SS2AssetRequest<MonsterAssetCollection> AssetRequest => SS2Assets.LoadAssetAsync<MonsterAssetCollection>("acMimic", SS2Bundle.Indev);
+
+        public static GameObject _masterPrefab;
+
+        public override void Initialize()
+        {
+            _masterPrefab = AssetCollection.FindAsset<GameObject>("MimicMaster");
+        }
+
+        public override bool IsAvailable(ContentPack contentPack)
+        {
+            return true;
+        }
+
+    }
+}
