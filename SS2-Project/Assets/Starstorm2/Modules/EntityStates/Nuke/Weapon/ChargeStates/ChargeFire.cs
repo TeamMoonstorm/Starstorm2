d234d400d5493a84c4e51076794375c37cb2e580
hevia <anthony@hevia.dev>
2024-08-06 21:53:37 -0700

    re-adding nucleator

--- SS2-Project/Assets/Starstorm2/Modules/EntityStates/Nuke/Weapon/ChargeStates/ChargeFire.cs
+++ SS2-Project/Assets/Starstorm2/Modules/EntityStates/Nuke/Weapon/ChargeStates/ChargeFire.cs
@@ -1,16 +1,16 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace EntityStates.Nuke.Weapon
-//{
-//    public class ChargeFire : BaseNukeChargeState
-//    {
-//        protected override SS2.Survivors.Nuke.IChargedState GetFireState()
-//        {
-//            return new FireRadonFire();
-//        }
-//    }
-//}
+namespace EntityStates.Nuke.Weapon
+{
+    public class ChargeFire : BaseNukeChargeState
+    {
+        protected override SS2.Survivors.Nuke.IChargedState GetFireState()
+        {
+            return new FireRadonFire();
+        }
+    }
+}
--- SS2-Project/Assets/Starstorm2/Modules/EntityStates/Nuke/Weapon/ChargeStates/ChargeLaunch.cs
+++ SS2-Project/Assets/Starstorm2/Modules/EntityStates/Nuke/Weapon/ChargeStates/ChargeLaunch.cs
@@ -1,17 +1,17 @@
-﻿//using RoR2;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace EntityStates.Nuke
-//{
-//    public class ChargeLaunch : BaseNukeChargeState
-//    {
-//        protected override SS2.Survivors.Nuke.IChargedState GetFireState()
-//        {
-//            return new FireFusionLaunch();
-//        }
-//    }
-//}
+namespace EntityStates.Nuke
+{
+    public class ChargeLaunch : BaseNukeChargeState
+    {
+        protected override SS2.Survivors.Nuke.IChargedState GetFireState()
+        {
+            return new FireFusionLaunch();
+        }
+    }
+}
