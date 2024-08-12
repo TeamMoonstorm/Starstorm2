42484c77af1a31fe08711339d8daf62bea7013d6
swuff-star <63352264+swuff-star@users.noreply.github.com>
2024-08-12 00:15:26 -0400

    rule icons + lang tweaks

--- SS2-Project/Assets/Starstorm2/Modules/Util/RulebookEnabler.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Util/RulebookEnabler.cs
@@ -50,7 +50,7 @@ public class RulebookEnabler : MonoBehaviour
             playerDamageRule.category = ruleCategoryDef;
 
             ExtendedRuleChoiceDef defaultDamageChoice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRuleDefault", "DamageDefault");
-            defaultDamageChoice.sprite = null;
+            defaultDamageChoice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageDefault", SS2Bundle.Base);
             defaultDamageChoice.tooltipNameToken = "SS2_PLAYER_DAMAGE_DEFAULT_NAME_TOKEN";
             defaultDamageChoice.tooltipNameColor = color;
             defaultDamageChoice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_DEFAULT_BODY_TOKEN";
@@ -58,56 +58,56 @@ public class RulebookEnabler : MonoBehaviour
             playerDamageRule.MakeNewestChoiceDefault();
 
             ExtendedRuleChoiceDef increaseDamage25Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule25Increase", "25DamageIncrease");
-            increaseDamage25Choice.sprite = null;
+            increaseDamage25Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageIncrease25", SS2Bundle.Base);
             increaseDamage25Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_25_INCREASE_NAME_TOKEN";
             increaseDamage25Choice.tooltipNameColor = color;
             increaseDamage25Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_25_INCREASE_BODY_TOKEN";
             increaseDamage25Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef increaseDamage50Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule50Increase", "50DamageIncrease");
-            increaseDamage50Choice.sprite = null;
+            increaseDamage50Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageIncrease50", SS2Bundle.Base);
             increaseDamage50Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_50_INCREASE_NAME_TOKEN";
             increaseDamage50Choice.tooltipNameColor = color;
             increaseDamage50Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_50_INCREASE_BODY_TOKEN";
             increaseDamage50Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef increaseDamage75Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule75Increase", "75DamageIncrease");
-            increaseDamage75Choice.sprite = null;
+            increaseDamage75Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageIncrease75", SS2Bundle.Base);
             increaseDamage75Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_75_INCREASE_NAME_TOKEN";
             increaseDamage75Choice.tooltipNameColor = color;
             increaseDamage75Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_75_INCREASE_BODY_TOKEN";
             increaseDamage75Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef increaseDamage100Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule100Increase", "100DamageIncrease");
-            increaseDamage100Choice.sprite = null;
+            increaseDamage100Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageIncrease100", SS2Bundle.Base);
             increaseDamage100Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_100_INCREASE_NAME_TOKEN";
             increaseDamage100Choice.tooltipNameColor = color;
             increaseDamage100Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_100_INCREASE_BODY_TOKEN";
             increaseDamage100Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseDamage25Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule25Decrease", "25DamageDecrease");
-            decreaseDamage25Choice.sprite = null;
+            decreaseDamage25Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageDecrease25", SS2Bundle.Base);
             decreaseDamage25Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_25_DECREASE_NAME_TOKEN";
             decreaseDamage25Choice.tooltipNameColor = color;
             decreaseDamage25Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_25_DECREASE_BODY_TOKEN";
             decreaseDamage25Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseDamage50Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule50Decrease", "50DamageDecrease");
-            decreaseDamage50Choice.sprite = null;
+            decreaseDamage50Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageDecrease50", SS2Bundle.Base);
             decreaseDamage50Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_50_DECREASE_NAME_TOKEN";
             decreaseDamage50Choice.tooltipNameColor = color;
             decreaseDamage50Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_50_DECREASE_BODY_TOKEN";
             decreaseDamage50Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseDamage75Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule75Decrease", "75DamageDecrease");
-            decreaseDamage75Choice.sprite = null;
+            decreaseDamage75Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageDecrease75", SS2Bundle.Base);
             decreaseDamage75Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_75_DECREASE_NAME_TOKEN";
             decreaseDamage75Choice.tooltipNameColor = color;
             decreaseDamage75Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_75_DECREASE_BODY_TOKEN";
             decreaseDamage75Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseDamage90Choice = playerDamageRule.AddExtendedRuleChoiceDef("ss2PlayerDamageRule90Decrease", "90DamageDecrease");
-            decreaseDamage90Choice.sprite = null;
+            decreaseDamage90Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleDamageDecrease90", SS2Bundle.Base);
             decreaseDamage90Choice.tooltipNameToken = "SS2_PLAYER_DAMAGE_90_DECREASE_NAME_TOKEN";
             decreaseDamage90Choice.tooltipNameColor = color;
             decreaseDamage90Choice.tooltipBodyToken = "SS2_PLAYER_DAMAGE_90_DECREASE_BODY_TOKEN";
@@ -124,7 +124,7 @@ public class RulebookEnabler : MonoBehaviour
 
             //misleading choice names but whatever
             ExtendedRuleChoiceDef defaultArmorChoice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRuleDefault", "Default");
-            defaultArmorChoice.sprite = null;
+            defaultArmorChoice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorDefault", SS2Bundle.Base);
             defaultArmorChoice.tooltipNameToken = "SS2_PLAYER_ARMOR_DEFAULT_NAME_TOKEN";
             defaultArmorChoice.tooltipNameColor = color;
             defaultArmorChoice.tooltipBodyToken = "SS2_PLAYER_ARMOR_DEFAULT_BODY_TOKEN";
@@ -132,56 +132,56 @@ public class RulebookEnabler : MonoBehaviour
             playerArmorRule.MakeNewestChoiceDefault();
 
             ExtendedRuleChoiceDef increaseArmor25Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule25Increase", "25ArmorIncrease");
-            increaseArmor25Choice.sprite = null;
+            increaseArmor25Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorIncrease25", SS2Bundle.Base);
             increaseArmor25Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_25_INCREASE_NAME_TOKEN";
             increaseArmor25Choice.tooltipNameColor = color;
             increaseArmor25Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_25_INCREASE_BODY_TOKEN";
             increaseArmor25Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef increaseArmor50Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule50Increase", "50ArmorIncrease");
-            increaseArmor50Choice.sprite = null;
+            increaseArmor50Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorIncrease50", SS2Bundle.Base);
             increaseArmor50Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_50_INCREASE_NAME_TOKEN";
             increaseArmor50Choice.tooltipNameColor = color;
             increaseArmor50Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_50_INCREASE_BODY_TOKEN";
             increaseArmor50Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef increaseArmor75Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule75Increase", "75ArmorIncrease");
-            increaseArmor75Choice.sprite = null;
+            increaseArmor75Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorIncrease75", SS2Bundle.Base);
             increaseArmor75Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_75_INCREASE_NAME_TOKEN";
             increaseArmor75Choice.tooltipNameColor = color;
             increaseArmor75Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_75_INCREASE_BODY_TOKEN";
             increaseArmor75Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef increaseArmor100Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule100Increase", "100ArmorIncrease");
-            increaseArmor100Choice.sprite = null;
+            increaseArmor100Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorIncrease100", SS2Bundle.Base);
             increaseArmor100Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_100_INCREASE_NAME_TOKEN";
             increaseArmor100Choice.tooltipNameColor = color;
             increaseArmor100Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_100_INCREASE_BODY_TOKEN";
             increaseArmor100Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseArmor25Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule25Decrease", "25ArmorDecrease");
-            decreaseArmor25Choice.sprite = null;
+            decreaseArmor25Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorDecrease25", SS2Bundle.Base);
             decreaseArmor25Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_25_DECREASE_NAME_TOKEN";
             decreaseArmor25Choice.tooltipNameColor = color;
             decreaseArmor25Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_25_DECREASE_BODY_TOKEN";
             decreaseArmor25Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseArmor50Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule50Decrease", "50ArmorDecrease");
-            decreaseArmor50Choice.sprite = null;
+            decreaseArmor50Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorDecrease50", SS2Bundle.Base);
             decreaseArmor50Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_50_DECREASE_NAME_TOKEN";
             decreaseArmor50Choice.tooltipNameColor = color;
             decreaseArmor50Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_50_DECREASE_BODY_TOKEN";
             decreaseArmor50Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseArmor75Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule75Decrease", "75ArmorDecrease");
-            decreaseArmor75Choice.sprite = null;
+            decreaseArmor75Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorDecrease75", SS2Bundle.Base);
             decreaseArmor75Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_75_DECREASE_NAME_TOKEN";
             decreaseArmor75Choice.tooltipNameColor = color;
             decreaseArmor75Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_75_DECREASE_BODY_TOKEN";
             decreaseArmor75Choice.excludeByDefault = false;
 
             ExtendedRuleChoiceDef decreaseArmor90Choice = playerArmorRule.AddExtendedRuleChoiceDef("ss2PlayerArmorRule90Decrease", "90ArmorDecrease");
-            decreaseArmor90Choice.sprite = null;
+            decreaseArmor90Choice.sprite = SS2Assets.LoadAsset<Sprite>("texIconRuleArmorDecrease90", SS2Bundle.Base);
             decreaseArmor90Choice.tooltipNameToken = "SS2_PLAYER_ARMOR_90_DECREASE_NAME_TOKEN";
             decreaseArmor90Choice.tooltipNameColor = color;
             decreaseArmor90Choice.tooltipBodyToken = "SS2_PLAYER_ARMOR_90_DECREASE_BODY_TOKEN";
