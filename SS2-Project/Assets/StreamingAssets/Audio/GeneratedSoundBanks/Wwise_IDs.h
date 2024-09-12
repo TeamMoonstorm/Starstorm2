a4c264d4a163e402538710e42154f893dd83bbb9
nayDPzz <88465294+nayDPz@users.noreply.github.com>
2024-09-12 13:36:28 -0400

    generate soundbanks
    it said theres an error gonna pretend i didnt see it

---
+++ SS2-Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,215 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ACTIVATEBLOODTESTER = 4040666791U;
+        static const AkUniqueID AUGURY = 202915568U;
+        static const AkUniqueID CHIRRDASHSTART = 874728115U;
+        static const AkUniqueID CHIRRFIREPRIMARY = 1172202385U;
+        static const AkUniqueID CHIRRFIREPRIMARYSINGLE = 959550553U;
+        static const AkUniqueID CHIRRFIRESPECIAL = 295786462U;
+        static const AkUniqueID CHIRRFIRESPITBOMB = 709034819U;
+        static const AkUniqueID CHIRRGRABFLYUP = 649273493U;
+        static const AkUniqueID CHIRRGRABSTART = 1440739445U;
+        static const AkUniqueID CHIRRGRABTHROW = 3463916741U;
+        static const AkUniqueID CHIRRLEAFHIT = 2661831180U;
+        static const AkUniqueID CHIRRSPITBOMBIMPACT = 670255117U;
+        static const AkUniqueID CHIRRSPRINTLOOP = 2730174275U;
+        static const AkUniqueID CHIRRSPRINTSTART = 3002414883U;
+        static const AkUniqueID CHIRRSPRINTSTOP = 1060087585U;
+        static const AkUniqueID CHIRRSTEP = 3251339703U;
+        static const AkUniqueID CHIRRTHROWHITGROUND = 4180439307U;
+        static const AkUniqueID CRYPTICSOURCEFIRE = 3284465828U;
+        static const AkUniqueID CRYPTICSOURCESTAGE1 = 2289766871U;
+        static const AkUniqueID CRYPTICSOURCESTAGE2 = 2289766868U;
+        static const AkUniqueID CRYPTICSOURCESTAGE3 = 2289766869U;
+        static const AkUniqueID CYBORGPRIMARY = 3105092465U;
+        static const AkUniqueID CYBORGSECONDARY = 4193594557U;
+        static const AkUniqueID CYBORGSPECIALPLACE = 1373732423U;
+        static const AkUniqueID CYBORGSPECIALTELEPORT = 2734889455U;
+        static const AkUniqueID CYBORGUTILITY = 613000131U;
+        static const AkUniqueID DIARYLEVELUP = 3870731187U;
+        static const AkUniqueID DROIDHEAD = 3118940775U;
+        static const AkUniqueID ETHEREALACTIVAGLOBAL = 3698808448U;
+        static const AkUniqueID ETHEREALACTIVATE = 2370200216U;
+        static const AkUniqueID ETHEREALBELL = 2401444224U;
+        static const AkUniqueID ETHEREALIDLE = 4294882297U;
+        static const AkUniqueID ETHEREALPILLARIDLE = 1367909893U;
+        static const AkUniqueID ETHEREALPILLARIMPACT = 4148654057U;
+        static const AkUniqueID ETHEREALPILLARSPAWN = 2943830540U;
+        static const AkUniqueID ETHEREALPILLARSPAWN2 = 1154060502U;
+        static const AkUniqueID ETHEREALSPAWN = 4032524928U;
+        static const AkUniqueID EXECUTIONERGAINCHARGE = 566193013U;
+        static const AkUniqueID EXECUTIONERMAXCHARGE = 1925425774U;
+        static const AkUniqueID EXECUTIONERPRIMARY = 953642894U;
+        static const AkUniqueID EXECUTIONERPRIMARYCRIT = 429550690U;
+        static const AkUniqueID EXECUTIONERSECONDARY = 2401513682U;
+        static const AkUniqueID EXECUTIONERSPECIALCAST = 1499433712U;
+        static const AkUniqueID EXECUTIONERSPECIALIMPACT = 2308605527U;
+        static const AkUniqueID EXECUTIONERSUPERCHARGE = 3396808549U;
+        static const AkUniqueID EXECUTIONERUTILITY = 2830854628U;
+        static const AkUniqueID FOLLOWERCAST = 3057152170U;
+        static const AkUniqueID FOLLOWERIDLESTART = 2970410379U;
+        static const AkUniqueID FOLLOWERIDLESTOP = 2787513641U;
+        static const AkUniqueID FOLLOWERSPAWN = 134703416U;
+        static const AkUniqueID FOLLOWERVO = 2589904694U;
+        static const AkUniqueID GREATERWARBANNER = 2901870699U;
+        static const AkUniqueID HOTTESTSAUCE = 571024191U;
+        static const AkUniqueID JETBOOTSEXPLOSION = 1235706518U;
+        static const AkUniqueID LAMPBULLET = 1316900923U;
+        static const AkUniqueID LAMPIMPACT = 1573671621U;
+        static const AkUniqueID LIGHTNING_AUGURY = 2831146217U;
+        static const AkUniqueID MOLTENCOIN = 1681111803U;
+        static const AkUniqueID NEMHUNTRESSBOWMAX = 1389751463U;
+        static const AkUniqueID NEMHUNTRESSCHARGEBOW = 415529083U;
+        static const AkUniqueID NEMHUNTRESSFIREBOW = 3663148423U;
+        static const AkUniqueID NEMMANDOCHARACTERSELECT = 3733468431U;
+        static const AkUniqueID NEMMANDOCHARGEBEAM = 2165644167U;
+        static const AkUniqueID NEMMANDOCHARGEBEAM2 = 3141162423U;
+        static const AkUniqueID NEMMANDOCHARGEDECISIVE = 2657646628U;
+        static const AkUniqueID NEMMANDOCSS = 1177423987U;
+        static const AkUniqueID NEMMANDODECISIVESTRIKECHARGE = 1250309938U;
+        static const AkUniqueID NEMMANDODECISIVESTRIKEFIRE = 430199870U;
+        static const AkUniqueID NEMMANDODECISIVESTRIKEREADY = 618420807U;
+        static const AkUniqueID NEMMANDOFIREBEAM = 1710620195U;
+        static const AkUniqueID NEMMANDOFIREBEAM2 = 2772373803U;
+        static const AkUniqueID NEMMANDOGUNSPIN = 615714056U;
+        static const AkUniqueID NEMMANDOJCECLICK = 2276677644U;
+        static const AkUniqueID NEMMANDORELOADGUN = 1862847105U;
+        static const AkUniqueID NEMMANDOSABERHIT = 82574964U;
+        static const AkUniqueID NEMMANDOSHEATHSWORD = 950548496U;
+        static const AkUniqueID NEMMANDOSHOOT = 4278514587U;
+        static const AkUniqueID NEMMANDOSHOOTCLASSIC = 3671834949U;
+        static const AkUniqueID NEMMANDOSHOOTCRIT = 2015914139U;
+        static const AkUniqueID NEMMANDOSHOOTGUN = 3892678859U;
+        static const AkUniqueID NEMMANDOSUBMISSIONCHARGE = 2068834138U;
+        static const AkUniqueID NEMMANDOSUBMISSIONFIRE = 752795878U;
+        static const AkUniqueID NEMMANDOSUBMISSIONREADY = 3331522399U;
+        static const AkUniqueID NEMMANDOSWING = 3008432276U;
+        static const AkUniqueID NEMMANDOSWINGCRIT = 3153874552U;
+        static const AkUniqueID NEMMANDOSWINGDECISIVE = 1642533830U;
+        static const AkUniqueID NEMMANDOSWINGSABER = 113243767U;
+        static const AkUniqueID NEMMANDOSWINGSABERDECISIVE = 1052053089U;
+        static const AkUniqueID NUCLEATORCHARGEPRIMARY = 420124658U;
+        static const AkUniqueID PICKUPCOFFEEBEAN = 4059362439U;
+        static const AkUniqueID PLAY_CHIRRCSS = 2871870429U;
+        static const AkUniqueID PLAY_CHIRRSPITBOMBLOOP = 2747610824U;
+        static const AkUniqueID PLAY_ETHEREAL_FIREPILLAR_LOOP = 1371804952U;
+        static const AkUniqueID PLAY_NEMMERC_CLONE_SPAWN = 3212416563U;
+        static const AkUniqueID PLAY_NEMMERC_DASH = 280596056U;
+        static const AkUniqueID PLAY_NEMMERC_DASH_START = 1211800949U;
+        static const AkUniqueID PLAY_NEMMERC_KNIFE_IMPACT = 1731428284U;
+        static const AkUniqueID PLAY_NEMMERC_KNIFE_RIP = 646660983U;
+        static const AkUniqueID PLAY_NEMMERC_KNIFE_THROW = 2890061578U;
+        static const AkUniqueID PLAY_NEMMERC_PRIMARY_FIRE = 2406586433U;
+        static const AkUniqueID PLAY_NEMMERC_PRIMARY_FIRE2 = 282242657U;
+        static const AkUniqueID PLAY_NEMMERC_PRIMARY_FIRE3 = 282242656U;
+        static const AkUniqueID PLAY_NEMMERC_PRIMARY_RELOAD = 1171975898U;
+        static const AkUniqueID PLAY_NEMMERC_SECONDARY_LUNGE = 576705642U;
+        static const AkUniqueID PLAY_NEMMERC_SPECIAL = 428808189U;
+        static const AkUniqueID PLAY_NEMMERC_UTILITY_ENTER = 1611310971U;
+        static const AkUniqueID PLAY_NEMMERC_UTILITY_EXIT = 3128507003U;
+        static const AkUniqueID PLAY_SOFT_RAIN = 263522455U;
+        static const AkUniqueID PLAY_SS2_MUSIC_SYSTEM = 3480457898U;
+        static const AkUniqueID PROCAMULET = 3464571447U;
+        static const AkUniqueID PROCERRATICGADGETCHAIN = 3050313720U;
+        static const AkUniqueID PROCERRATICGADGETLIGHTNING = 3125516253U;
+        static const AkUniqueID PROCLIGHTNINGONKILL = 3105193662U;
+        static const AkUniqueID PROCSPEAKER = 2402360908U;
+        static const AkUniqueID PROCTREMATODES = 1520542387U;
+        static const AkUniqueID REFABRICATORACTION = 2808251015U;
+        static const AkUniqueID REMUNERATIONSPAWN = 159540939U;
+        static const AkUniqueID REMUNERATIONSPAWN2 = 3276263843U;
+        static const AkUniqueID SIGILACTIVATION = 2800331071U;
+        static const AkUniqueID SPAWNMGR = 2187217924U;
+        static const AkUniqueID SS_WINDAMBIENCE = 892248604U;
+        static const AkUniqueID STIRRINGSOUL = 3982623506U;
+        static const AkUniqueID STOP_CHIRRCSS = 3156814475U;
+        static const AkUniqueID STOP_CHIRRSPITBOMBLOOP = 1032313774U;
+        static const AkUniqueID STOP_ETHEREAL_FIREPILLAR_LOOP = 2875515542U;
+        static const AkUniqueID STOP_SOFT_RAIN = 4234730185U;
+        static const AkUniqueID STRANGECAN = 3750145159U;
+        static const AkUniqueID SWIFTSKATEBOARD = 961366952U;
+        static const AkUniqueID UNIVERSALCHARGER = 1435965638U;
+        static const AkUniqueID VOIDAMBIENT = 2533932213U;
+        static const AkUniqueID WASTELANDERPRIMARY = 2333188815U;
+        static const AkUniqueID WASTELANDERPRIMARYCRIT = 1319272055U;
+        static const AkUniqueID WASTELANDERSECONDARY = 342360847U;
+        static const AkUniqueID WAYFARERATTACK = 2495495812U;
+        static const AkUniqueID WAYFARERDEATH = 2635526886U;
+        static const AkUniqueID WAYFARERIDLESTART = 2250290942U;
+        static const AkUniqueID WAYFARERIDLESTOP = 3127064350U;
+        static const AkUniqueID WAYFARERSPAWN = 1131808197U;
+        static const AkUniqueID WAYFARERVO = 2393435613U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMEPLAYSONGCHOICE
+        {
+            static const AkUniqueID GROUP = 1741660947U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SONG_NEMESIS_END = 818387697U;
+                static const AkUniqueID SONG_NEMESIS_MAIN = 617747445U;
+                static const AkUniqueID SONG_NEMESIS_MAIN_B = 4271331250U;
+                static const AkUniqueID SONG_NEMESIS_START = 231224994U;
+            } // namespace STATE
+        } // namespace GAMEPLAYSONGCHOICE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MUSICEV
+        {
+            static const AkUniqueID GROUP = 3897813747U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace MUSICEV
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYERHEALTH = 151362964U;
+        static const AkUniqueID VOLUME_MSX = 3729143042U;
+        static const AkUniqueID VOLUME_SFX = 3673881719U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SS2MUSIC = 2545819644U;
+        static const AkUniqueID STARSTORM2 = 866530374U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
