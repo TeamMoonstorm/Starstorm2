3ee3ee94b0d5f03891003172ee3edb8885e69447
swuff-star <63352264+swuff-star@users.noreply.github.com>
2024-09-21 13:22:09 -0400

    various fixe
    tweaked empy spawning (cheaper, should no longer be on moon)
    cap gm now has hat on head
    lang files for enemies
    fixed malice unlock
    strange can unlock desc restored
    blacklisted termatode, reactor, speakers
    nemcommando sword no longer purpl after dec. strike
    removed text about nemcommando reload
    adding stunning keyword to dec strike desc
    fixed nemcommando pink sword
    tweaked exe orb gain values for new default
    restored+'fixed' erratic gadget unlock

--- SS2-Project/Assets/Starstorm2/Modules/Unlockables/Items/ErraticGadetAchievement.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Unlockables/Items/ErraticGadetAchievement.cs
@@ -7,69 +7,71 @@ namespace SS2.Unlocks.Pickups
     {
         private Inventory currentInventory;
         private PlayerCharacterMasterController currentMasterController;
-        public static List<ItemDef> CritItems = new List<ItemDef>();
-        //public override void OnInstall()
-        //{
-        //    base.OnInstall();
-        //    localUser.onMasterChanged += OnMasterChanged;
-        //    SetMasterController(localUser.cachedMasterController);
+        public static List<ItemDef> LightningItems = new List<ItemDef>();
+        public override void OnInstall()
+        {
+            base.OnInstall();
+            localUser.onMasterChanged += OnMasterChanged;
+            SetMasterController(localUser.cachedMasterController);
 
-        //    CritItems.Add(RoR2Content.Items.CritGlasses);
-        //    CritItems.Add(RoR2Content.Items.AttackSpeedOnCrit);
-        //    CritItems.Add(RoR2Content.Items.HealOnCrit);
-        //    CritItems.Add(RoR2Content.Items.BleedOnHitAndExplode);
-        //    if (SS2Content.Items.Needles)
-        //        CritItems.Add(SS2Content.Items.Needles);
-        //    if (SS2Content.Items.GreenChocolate)
-        //        CritItems.Add(SS2Content.Items.GreenChocolate);
-        //    if (SS2Content.Items.HuntersSigil)
-        //        CritItems.Add(SS2Content.Items.HuntersSigil);
-        //}
+            LightningItems.Add(RoR2Content.Items.ChainLightning);
+            LightningItems.Add(RoR2Content.Items.LightningStrikeOnHit);
+            LightningItems.Add(RoR2Content.Items.ShockNearby);
+            LightningItems.Add(DLC1Content.Items.ChainLightningVoid);
+            if (SS2Content.Items.CrypticSource)
+                LightningItems.Add(SS2Content.Items.CrypticSource);
+            if (SS2Content.Items.LightningOnKill)
+                LightningItems.Add(SS2Content.Items.LightningOnKill);
+        }
 
-        //public override void OnUninstall()
-        //{
-        //    SetMasterController(null);
-        //    localUser.onMasterChanged -= OnMasterChanged;
-        //    base.OnUninstall();
-        //}
-        //private void SetMasterController(PlayerCharacterMasterController newMasterController)
-        //{
-        //    if ((object)currentMasterController != newMasterController)
-        //    {
-        //        if ((object)currentInventory != null)
-        //        {
-        //            currentInventory.onInventoryChanged -= OnInventoryChanged;
-        //        }
-        //        currentMasterController = newMasterController;
-        //        currentInventory = currentMasterController?.master?.inventory;
-        //        if ((object)currentInventory != null)
-        //        {
-        //            currentInventory.onInventoryChanged += OnInventoryChanged;
-        //        }
-        //    }
-        //}
-        //private void OnInventoryChanged()
-        //{
-        //    if ((bool)currentInventory)
-        //    {
-        //        int neededItemsObtained = 0;
-        //        foreach (ItemDef critItem in CritItems)
-        //        {
-        //            var currentItemCount = currentInventory.GetItemCount(critItem);
-        //            if (currentItemCount >= 1)
-        //            {
-        //                neededItemsObtained += currentItemCount;
-        //            }
-        //        }
-        //        if (neededItemsObtained >= 10)
-        //        {
-        //            Grant();
-        //        }
-        //    }
-        //}
-        //private void OnMasterChanged()
-        //{
-        //    SetMasterController(localUser.cachedMasterController);
-        //}
+        public override void OnUninstall()
+        {
+            SetMasterController(null);
+            localUser.onMasterChanged -= OnMasterChanged;
+            base.OnUninstall();
+        }
+        private void SetMasterController(PlayerCharacterMasterController newMasterController)
+        {
+            if ((object)currentMasterController != newMasterController)
+            {
+                if ((object)currentInventory != null)
+                {
+                    currentInventory.onInventoryChanged -= OnInventoryChanged;
+                }
+                currentMasterController = newMasterController;
+                currentInventory = currentMasterController?.master?.inventory;
+                if ((object)currentInventory != null)
+                {
+                    currentInventory.onInventoryChanged += OnInventoryChanged;
+                }
+            }
+        }
+        private void OnInventoryChanged()
+        {
+            if ((bool)currentInventory)
+            {
+                int neededItemsObtained = 0;
+                foreach (ItemDef lightningItem in LightningItems)
+                {
+                    var currentItemCount = currentInventory.GetItemCount(lightningItem);
+                    if (currentItemCount >= 1)
+                    {
+                        neededItemsObtained += currentItemCount;
+                    }
+                }
+                if (currentInventory.currentEquipmentIndex == RoR2Content.Equipment.Lightning.equipmentIndex || currentInventory.currentEquipmentIndex == RoR2Content.Equipment.AffixBlue.equipmentIndex)
+                {
+                    neededItemsObtained += 1;
+                }
+                if (neededItemsObtained >= 5)
+                {
+                    Grant();
+                }
+            }
+        }
+        private void OnMasterChanged()
+        {
+            SetMasterController(localUser.cachedMasterController);
+        }
     }
 }
