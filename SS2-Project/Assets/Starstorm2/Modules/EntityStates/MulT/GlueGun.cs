5e7a6dcde50e681eba76ce7894bc2046f0144cd7
hevia <anthony@hevia.dev>
2024-10-06 23:14:50 -0700

    Glue gun for all the people who ate glue growing up

--- SS2-Project/Assets/Starstorm2/Modules/EntityStates/MulT/GlueGun.cs
+++ SS2-Project/Assets/Starstorm2/Modules/EntityStates/MulT/GlueGun.cs
@@ -1,20 +1,72 @@
 ﻿using EntityStates;
+using RoR2.Projectile;
+using RoR2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace EntityStates.MulT
 {
     public class GlueGun : BaseSkillState
     {
-        public float baseDuration = 0.5f;
+        // TODO: Move this to acrid class
+        public GameObject effectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Croco/MuzzleflashCroco.prefab").WaitForCompletion();
+
+        [SerializeField]
+        public float baseDuration = 2f;
+
+        [SerializeField]
+        public float damageCoefficient = 0.7f;
+
+        [SerializeField]
+        public float force = 20f;
+
+        [SerializeField]
+        public string attackString;
+
+        [SerializeField]
+        public float recoilAmplitude;
+
+        [SerializeField]
+        public float bloom;
+
         private float duration;
 
         public override void OnEnter()
         {
             base.OnEnter();
+            Ray aimRay = GetAimRay();
+            duration = baseDuration / attackSpeedStat;
+            StartAimMode(duration + 2f);
+            PlayAnimation("Gesture, Mouth", "FireSpit", "FireSpit.playbackRate", duration);
+            Util.PlaySound(attackString, base.gameObject);
+            AddRecoil(-1f * recoilAmplitude, -1.5f * recoilAmplitude, -0.25f * recoilAmplitude, 0.25f * recoilAmplitude);
+            base.characterBody.AddSpreadBloom(bloom);
+            string muzzleName = "MouthMuzzle";
+
+            if (effectPrefab)
+            {
+                EffectManager.SimpleMuzzleFlash(effectPrefab, base.gameObject, muzzleName, transmit: false);
+            }
+
+            if (base.isAuthority)
+            {
+                FireProjectileInfo fireProjectileInfo = default(FireProjectileInfo);
+                fireProjectileInfo.damageTypeOverride = DamageType.Generic;
+                fireProjectileInfo.projectilePrefab = SS2.Survivors.Acrid.corrodingSpitProjectilePrefab;
+                fireProjectileInfo.position = aimRay.origin;
+                fireProjectileInfo.rotation = Util.QuaternionSafeLookRotation(aimRay.direction);
+                fireProjectileInfo.owner = base.gameObject;
+                fireProjectileInfo.damage = damageStat * damageCoefficient;
+                fireProjectileInfo.force = force;
+                fireProjectileInfo.crit = Util.CheckRoll(critStat, base.characterBody.master);
+
+                ProjectileManager.instance.FireProjectile(fireProjectileInfo);
+            }
         }
 
         public override void OnExit()
