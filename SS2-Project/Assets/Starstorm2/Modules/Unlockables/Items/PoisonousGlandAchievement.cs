7892a99c83ba3c6dca8f5c340fe6d118c70906b6
nayDPzz <88465294+nayDPz@users.noreply.github.com>
2024-10-30 19:23:05 -0400

    cheevos!!!!!!!!

---
+++ SS2-Project/Assets/Starstorm2/Modules/Unlockables/Items/PoisonousGlandAchievement.cs
@@ -0,0 +1,67 @@
+﻿using RoR2;
+using RoR2.Achievements;
+using RoR2.Stats;
+namespace SS2.Unlocks.Pickups
+{
+    public sealed class PoisonousGlandAchievement : BaseAchievement
+    {
+        public static readonly StatDef crocoPoisonedEnemies = StatDef.Register("crocoPoisonEnemeisAchievementProgress", StatRecordType.Sum, StatDataType.ULong, 0.0, null);
+        public override void OnInstall()
+        {
+            base.OnInstall();          
+            base.userProfile.onStatsReceived += Check;
+            base.SetServerTracked(true);
+        }       
+        private void Check()
+        {
+            if (base.userProfile.statSheet.GetStatValueULong(crocoPoisonedEnemies) >= 500)
+            {
+                base.Grant();
+            }
+        }
+        public override float ProgressForAchievement()
+        {
+            return base.userProfile.statSheet.GetStatValueULong(crocoPoisonedEnemies) / 500;
+        }
+        public override void OnUninstall()
+        {
+            base.OnUninstall();           
+            base.userProfile.onStatsReceived -= Check;
+        }
+
+        public override BodyIndex LookUpRequiredBodyIndex()
+        {
+            return BodyCatalog.FindBodyIndex("CrocoBody");
+        }
+
+		private class PoisonousGlandServerAchievement : BaseServerAchievement
+		{
+			public override void OnInstall()
+			{
+				base.OnInstall();
+				this.crocoBodyIndex = BodyCatalog.FindBodyIndex("CrocoBody");
+                DotController.onDotInflictedServerGlobal += CheckCrocoPoison;
+            }
+
+			public override void OnUninstall()
+			{
+                DotController.onDotInflictedServerGlobal -= CheckCrocoPoison;
+                base.OnUninstall();
+			}
+            private void CheckCrocoPoison(DotController dotController, ref InflictDotInfo inflictDotInfo)
+            {
+                if (inflictDotInfo.dotIndex != DotController.DotIndex.Poison) return;
+                CharacterBody body = base.networkUser.GetCurrentBody();
+                if(base.IsCurrentBody(body) && body.bodyIndex == crocoBodyIndex)
+                {
+                    PlayerStatsComponent masterPlayerStatsComponent = base.networkUser.masterPlayerStatsComponent;
+                    if (masterPlayerStatsComponent)
+                    {
+                        masterPlayerStatsComponent.currentStats.PushStatValue(crocoPoisonedEnemies, 1);
+                    }
+                }
+            }
+            private BodyIndex crocoBodyIndex;
+		}
+	}
+}
---
+++ SS2-Project/Assets/Starstorm2/Modules/Unlockables/Items/UniversalChargerAchievement.cs
@@ -0,0 +1,74 @@
+﻿using RoR2;
+using RoR2.Achievements;
+using RoR2.Stats;
+namespace SS2.Unlocks.Pickups
+{	
+	public class UniversalChargerAchievement : BaseAchievement
+	{
+		public static readonly StatDef engiTurretKills = StatDef.Register("engiTurretKillsAchievementProgress", StatRecordType.Sum, StatDataType.ULong, 0.0, null);
+		public override BodyIndex LookUpRequiredBodyIndex()
+		{
+			return BodyCatalog.FindBodyIndex("EngiBody");
+		}
+
+		public override void OnBodyRequirementMet()
+		{
+			base.OnBodyRequirementMet();
+			base.userProfile.onStatsReceived += Check;
+			base.SetServerTracked(true);
+		}
+		public override float ProgressForAchievement()
+		{
+			return base.userProfile.statSheet.GetStatValueULong(engiTurretKills) / 400;
+		}
+		public override void OnBodyRequirementBroken()
+		{
+			base.SetServerTracked(false);
+			base.userProfile.onStatsReceived -= Check;
+			base.OnBodyRequirementBroken();
+		}
+		private void Check()
+		{
+			if (base.userProfile.statSheet.GetStatValueULong(engiTurretKills) >= 400)
+			{
+				base.Grant();
+			}
+		}
+
+		public class UniversalChargerServerAchievement : BaseServerAchievement
+		{
+			public override void OnInstall()
+			{
+				base.OnInstall();
+				turret1BodyIndex = BodyCatalog.FindBodyIndex("EngiTurretBody");
+				turret2BodyIndex = BodyCatalog.FindBodyIndex("EngiWalkerTurretBody");
+				GlobalEventManager.onCharacterDeathGlobal += this.OnCharacterDeath;
+			}
+
+			public override void OnUninstall()
+			{
+				GlobalEventManager.onCharacterDeathGlobal -= this.OnCharacterDeath;
+				base.OnUninstall();
+			}
+
+			private void OnCharacterDeath(DamageReport damageReport)
+			{
+				if (damageReport.attackerBodyIndex == turret1BodyIndex || damageReport.attackerBodyIndex == turret2BodyIndex)
+				{
+					CharacterMaster master = damageReport.attackerOwnerMaster;
+					if(master && IsCurrentBody(master.GetBody()))
+                    {
+						PlayerStatsComponent masterPlayerStatsComponent = base.networkUser.masterPlayerStatsComponent;
+						if (masterPlayerStatsComponent)
+						{
+							masterPlayerStatsComponent.currentStats.PushStatValue(engiTurretKills, 1);
+						}
+					}					
+				}
+			}
+
+			private BodyIndex turret1BodyIndex;
+			private BodyIndex turret2BodyIndex;
+		}
+	}
+}
