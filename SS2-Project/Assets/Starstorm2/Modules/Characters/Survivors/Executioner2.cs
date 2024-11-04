88395bb3ea865436b15e3fccd0df25007fc19be7
Nebby <nebby131999@gmail.com>
2024-11-04 17:57:12 -0300

    Talk Tuah

--- SS2-Project/Assets/Starstorm2/Modules/Characters/Survivors/Executioner2.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Characters/Survivors/Executioner2.cs
@@ -151,7 +151,7 @@ namespace SS2.Survivors
             orig(self, characterBody);
         }
 
-        private void HelpOOB(On.RoR2.TeleportHelper.orig_TeleportBody orig, CharacterBody body, Vector3 targetFootPosition)
+        private void HelpOOB(On.RoR2.TeleportHelper.orig_TeleportBody orig, CharacterBody body, Vector3 targetFootPosition, bool _)
         {
             var exc = body.gameObject.GetComponent<ExecutionerController>();
             if (exc)
@@ -162,7 +162,7 @@ namespace SS2.Survivors
                     targetFootPosition += new Vector3(0, 1, 0);
                 }
             }
-            orig(body, targetFootPosition);
+            orig(body, targetFootPosition, _);
         }
 
         private HealthComponent.HealthBarValues FearExecuteHealthbar(On.RoR2.HealthComponent.orig_GetHealthBarValues orig, HealthComponent self)
