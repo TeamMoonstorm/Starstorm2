4568210617a98c4d290bb4f91c2c77a23085a9b9
hevia <anthony@hevia.dev>
2025-08-07 22:24:56 -0700

    ahh

---
+++ SS2-Project/Assets/Starstorm2/SS2Telemetry.cs
@@ -0,0 +1,20 @@
+namespace SS2
+{
+    public class SS2Telemetry
+    {
+        public static void Initialize()
+        {
+            // Initialize telemetry system
+        }
+
+        public static void TrackEvent(string eventName, object eventData)
+        {
+            // Track an event
+        }
+
+        public static void TrackError(string errorMessage, Exception exception)
+        {
+            // Track an error
+        }
+    }
+}
