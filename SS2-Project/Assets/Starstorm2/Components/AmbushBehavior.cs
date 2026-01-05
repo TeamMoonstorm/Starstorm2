31e1f090843dd0588ed1aa7b7dd6d664009b97bc
nayDPz <nate.pulling@gmail.com>
2026-01-04 22:19:12 -0500

    pedestal

---
+++ SS2-Project/Assets/Starstorm2/Components/AmbushBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SS2
+{
+    [RequireComponent(typeof(CombatDirector))]
+    [RequireComponent(typeof(CombatSquad))]
+    public class AmbushBehavior : NetworkBehaviour
+    {
+        [RuntimeInitializeOnLoadMethod]
+        private static void Init()
+        {
+            SS2Log.Info("Initializing AmbushBehavior");
+            SceneDirector.onPrePopulateMonstersSceneServer += OnPrePopulateMonstersSceneServer;
+            TeleporterInteraction.onTeleporterBeginChargingGlobal += OnTeleporterBeginCharging;
+        }
+
+        private static void OnPrePopulateMonstersSceneServer(SceneDirector sceneDirector)
+        {
+            foreach (var ambushBehavior in InstanceTracker.GetInstancesList<AmbushBehavior>())
+            {
+                sceneDirector.ReduceMonsterCredits(Mathf.RoundToInt(ambushBehavior.monsterCredit * sceneDirectorMonsterCreditSubtractionCoefficient));
+            }
+        }
+
+        private static void OnTeleporterBeginCharging(TeleporterInteraction teleporterInteraction)
+        {
+            if (NetworkServer.active)
+            {
+                foreach (var ambushBehavior in InstanceTracker.GetInstancesList<AmbushBehavior>())
+                {
+                    ambushBehavior.SetActive(false);
+                }
+            }
+        }
+        public int baseMonsterCredit = 100;
+        private float monsterCredit
+        {
+            get
+            {
+                return baseMonsterCredit * Stage.instance.entryDifficultyCoefficient;
+            }
+        }
+        public int maxMonsterTypes = 2;
+        public int maxMonstersPerType = 6;
+        public static float sceneDirectorMonsterCreditSubtractionCoefficient = 0.75f;
+
+        public GameObject spawnPositionEffectPrefab;
+        
+        private DirectorCard[] chosenDirectorCards = Array.Empty<DirectorCard>();
+        private bool active = true;
+        // {You have been ambushed by }{___}{___ and ___}{___, ___, and ___}
+
+        private CombatDirector combatDirector;
+
+        private void Awake()
+        {
+            combatDirector = GetComponent<CombatDirector>();
+        }
+        private void OnEnable()
+        {
+            InstanceTracker.Add(this);
+        }
+        private void OnDisable()
+        {
+            InstanceTracker.Remove(this);
+        }
+        public void SetActive(bool newActive)
+        {
+            active = newActive;
+        }
+
+    }
+}
---
+++ SS2-Project/Assets/Starstorm2/Modules/Interactables/InteractableTypes/Pedestal.cs
@@ -0,0 +1,27 @@
+﻿using RoR2;
+using RoR2.ContentManagement;
+using R2API;
+using UnityEngine;
+using UnityEngine.Networking;
+namespace SS2.Interactables
+{
+    public sealed class Pedestal : SS2Interactable
+    {
+        public override SS2AssetRequest<InteractableAssetCollection> AssetRequest => SS2Assets.LoadAssetAsync<InteractableAssetCollection>("acPedestal", SS2Bundle.Interactables);
+        public override void Initialize()
+        {
+            IL.RoR2.UI.PingIndicator.RebuildPing += PingIndicator_RebuildPing;
+        }
+
+        private void PingIndicator_RebuildPing(MonoMod.Cil.ILContext il)
+        {
+            // TODO: check for PedestalBehavior component and add the pickup's name to the string
+        }
+
+        public override bool IsAvailable(ContentPack contentPack)
+        {
+            return SS2Config.enableBeta;
+        }
+
+    }
+}
