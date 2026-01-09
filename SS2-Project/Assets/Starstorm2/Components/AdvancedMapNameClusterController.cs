3ec62bb29186a52494a40ee5209682ac8e43ea2b
JaceCascade <66852270+JaceDaDorito@users.noreply.github.com>
2026-01-09 05:45:22 -0500

    TODO: Need a child locator for the MapCluster on HudSimple

---
+++ SS2-Project/Assets/Starstorm2/Components/AdvancedMapNameClusterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using RoR2;
+
+namespace SS2.Components
+{
+    public class AdvancedMapNameClusterController : MonoBehaviour
+    {
+
+        public void OnEnable()
+        {
+
+        }
+
+        public void OnDisable()
+        {
+
+        }
+
+    }
+}
