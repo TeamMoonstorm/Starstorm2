e659011f2f05bc60b4363b976a8120aa25d46446
JaceCascade <66852270+JaceDaDorito@users.noreply.github.com>
2026-01-09 03:38:48 -0500

    base

--- SS2-Project/Assets/Starstorm2/Modules/Scenes/SceneTypes/VoidShop.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Scenes/SceneTypes/VoidShop.cs
@@ -12,7 +12,7 @@ namespace SS2.Scenes
 {
     public sealed class VoidShop : SS2Scene
     {
-        public override SS2AssetRequest<SceneAssetCollection> assetRequest => SS2Assets.LoadAssetAsync<SceneAssetCollection>("acVoidShop", SS2Bundle.SharedStages);
+        public override SS2AssetRequest<SceneAssetCollection> assetRequest => SS2Assets.LoadAssetAsync<SceneAssetCollection>("ac2VoidShop", SS2Bundle.SharedStages);
 
         public override void Initialize()
         {
@@ -23,12 +23,13 @@ namespace SS2.Scenes
         public override void OnServerStageBegin(Stage stage)
         {
             //summon trader
-            var traderSummon = new MasterSummon();
+            
+            /*var traderSummon = new MasterSummon();
             traderSummon.position = new Vector3(-0.88f, 0.47f, 60.15f);
             traderSummon.rotation = new Quaternion(0f, 90f, 0f, 0f);
             traderSummon.masterPrefab = SS2Assets.LoadAsset<GameObject>("TraderMaster", SS2Bundle.Indev);
             traderSummon.teamIndexOverride = TeamIndex.Neutral;
-            traderSummon.Perform();
+            traderSummon.Perform();*/
         }
 
         public override bool IsAvailable(ContentPack contentPack)
