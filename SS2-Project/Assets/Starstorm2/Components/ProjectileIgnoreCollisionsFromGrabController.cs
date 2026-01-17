e1add9ba715690d11e6c53fea75bce9fd2e3a4b9
nayDPz <nate.pulling@gmail.com>
2026-01-17 15:11:16 -0500

    cleaning up stupid class might fix #721 probly not

--- SS2-Project/Assets/Starstorm2/Components/ProjectileIgnoreCollisionsFromGrabController.cs
+++ SS2-Project/Assets/Starstorm2/Components/ProjectileIgnoreCollisionsFromGrabController.cs
@@ -2,40 +2,43 @@
 using UnityEngine;
 using RoR2.Projectile;
 using RoR2;
-using System.Linq;
+using System.Collections.Generic;
 namespace SS2.Components
 {
     [RequireComponent(typeof(ProjectileController))]
     public class ProjectileIgnoreCollisionsFromGrabController : MonoBehaviour
     {
-        [NonSerialized]
-        public Collider[] collidersToIgnore;
         private Collider myCollider;
       
         // AAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         private void Start()
         {
-            GrabController grabController = base.GetComponent<ProjectileController>().owner.GetComponent<GrabController>();
+            ProjectileController controller = GetComponent<ProjectileController>();
+            GrabController grabController = controller.owner ? controller.owner.GetComponent<GrabController>() : null;
             this.myCollider = base.GetComponent<Collider>();
 
             if (grabController && !grabController.victimInfo.body)
             {
-                //SS2Log.Warning("ProjectileIgnoreCollisionsFromGrabController.Start: GrabController had no body");
                 return;
             }
-            if (grabController.victimInfo.body.teamComponent.teamIndex != base.GetComponent<TeamFilter>().teamIndex) return;
-            // O_O
-            this.collidersToIgnore = HG.ArrayUtils.Join(grabController.victimColliders, grabController.victimInfo.body.hurtBoxGroup.hurtBoxes.Select(h => h.GetComponent<Collider>()).ToArray());
-            if (grabController)
+            var victimBody = grabController.victimInfo.body;
+            if (victimBody.teamComponent.teamIndex != base.GetComponent<TeamFilter>().teamIndex)
             {
-                foreach (Collider collider in this.collidersToIgnore)
+                return;
+            }
+            if (!victimBody.hurtBoxGroup)
+            {
+                return;
+            }
+
+            for (int i = 0; i < victimBody.hurtBoxGroup.hurtBoxes.Length; i++)
+            {
+                var hurtBox = victimBody.hurtBoxGroup.hurtBoxes[i];
+                if (hurtBox && hurtBox.TryGetComponent(out Collider collider))
                 {
-                    if(collider) // not null checking these leads to a lot of issues idk.
-                        Physics.IgnoreCollision(myCollider, collider, true);
+                    Physics.IgnoreCollision(myCollider, collider, true);
                 }
             }
-            
-            
         }
     }
 }
