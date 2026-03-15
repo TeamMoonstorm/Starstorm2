66afee599f383bceae9248b117ac92b792a94cc5
nayDPz <nate.pulling@gmail.com>
2026-03-15 19:15:53 -0400

    ambush

--- SS2-Project/Assets/Starstorm2/Components/AmbushBehavior.cs
+++ SS2-Project/Assets/Starstorm2/Components/AmbushBehavior.cs
@@ -7,7 +7,6 @@ using UnityEngine.Networking;
 namespace SS2
 {
     [RequireComponent(typeof(CombatDirector))]
-    [RequireComponent(typeof(CombatSquad))]
     public class AmbushBehavior : NetworkBehaviour
     {
         [RuntimeInitializeOnLoadMethod]
@@ -17,12 +16,17 @@ namespace SS2
             SceneDirector.onPrePopulateMonstersSceneServer += OnPrePopulateMonstersSceneServer;
             TeleporterInteraction.onTeleporterBeginChargingGlobal += OnTeleporterBeginCharging;
         }
-
+        [ConCommand(commandName = "ambush_debug", flags = (ConVarFlags.Cheat), helpText = "Toggle Ambush debugging.")]
+        private static void CCToggleDebug(ConCommandArgs args)
+        {
+            debug = !debug;
+            SS2Log.Debug("Ambush debugging is now " + (debug ? "ON" : "OFF"));
+        }
         private static void OnPrePopulateMonstersSceneServer(SceneDirector sceneDirector)
         {
             foreach (var ambushBehavior in InstanceTracker.GetInstancesList<AmbushBehavior>())
             {
-                sceneDirector.ReduceMonsterCredits(Mathf.RoundToInt(ambushBehavior.monsterCredit * sceneDirectorMonsterCreditSubtractionCoefficient));
+                sceneDirector.ReduceMonsterCredits(Mathf.RoundToInt(ambushBehavior.totalMonsterCredit * sceneDirectorMonsterCreditSubtractionCoefficient));
             }
         }
 
@@ -36,41 +40,296 @@ namespace SS2
                 }
             }
         }
-        public int baseMonsterCredit = 100;
-        private float monsterCredit
+
+        private static bool debug;
+        public static float sceneDirectorMonsterCreditSubtractionCoefficient = 0.75f;
+        private static float maxRage = 100f;
+
+        public float radius = 50f;
+        public Transform debugIndicator;
+
+        [Header("Rage Values")]
+        public float passiveRageGain = 0.5f;
+        public float rageGainAtEdge = 6.67f;
+        public float rageGainAtCenter = 33f;
+        public float rageOnKill = 33f;
+        public float rageOnInteract = 75f;
+
+        [Header("Large Monsters")]
+        public CombatDirector largeMonsterDirector;
+        public int baseLargeMonsterCredit = 75;
+        public int minLargeMonsters = 1;
+        public int maxLargeMonsters = 4;
+        private float largeMonsterCredit
         {
             get
             {
-                return baseMonsterCredit * Stage.instance.entryDifficultyCoefficient;
+                return baseLargeMonsterCredit * Stage.instance.entryDifficultyCoefficient;
             }
         }
-        public int maxMonsterTypes = 2;
-        public int maxMonstersPerType = 6;
-        public static float sceneDirectorMonsterCreditSubtractionCoefficient = 0.75f;
+        [Header("Small Monsters")]
+        public CombatDirector smallMonsterDirector;
+        public int baseSmallMonsterCredit = 75;
+        public int minSmallMonsters = 5;
+        public int maxSmallMonsters = 9;
+        private float smallMonsterCredit
+        {
+            get
+            {
+                return baseSmallMonsterCredit * Stage.instance.entryDifficultyCoefficient;
+            }
+        }
+        private float totalMonsterCredit
+        {
+            get
+            {
+                return smallMonsterCredit + largeMonsterCredit;
+            }
+        }
+
+        private float currentRage;
+        private CharacterBody currentTarget;
+
+        private DirectorCard chosenSmallCard;
+        private DirectorCard chosenLargeCard;
 
         public GameObject spawnPositionEffectPrefab;
-        
-        private DirectorCard[] chosenDirectorCards = Array.Empty<DirectorCard>();
+        public GameObject activatorEffectPrefab;
+
+        private Xoroshiro128Plus rng;
         private bool active = true;
         // {You have been ambushed by }{___}{___ and ___}{___, ___, and ___}
 
-        private CombatDirector combatDirector;
-
+        
+        
         private void Awake()
         {
-            combatDirector = GetComponent<CombatDirector>();
-        }
-        private void OnEnable()
-        {
             InstanceTracker.Add(this);
+
+            if (NetworkServer.active)
+            {
+                largeMonsterDirector.enabled = false;
+                smallMonsterDirector.enabled = false;
+                rng = new Xoroshiro128Plus(Run.instance.stageRng.nextUint);
+            }
+            
         }
-        private void OnDisable()
+
+        private void Start()
+        {
+            if (NetworkServer.active)
+            {
+                chosenSmallCard = SelectMonsterCard(smallMonsterCredit, maxSmallMonsters, minSmallMonsters);
+                chosenLargeCard = SelectMonsterCard(largeMonsterCredit, maxLargeMonsters, minLargeMonsters);
+            }
+        }
+        private void OnDestroy()
         {
             InstanceTracker.Remove(this);
         }
+        private void OnEnable()
+        {
+            GlobalEventManager.OnInteractionsGlobal += OnInteractionGlobal;
+            GlobalEventManager.onCharacterDeathGlobal += OnCharacterDeathGlobal;
+        }
+        private void OnDisable()
+        {
+            GlobalEventManager.OnInteractionsGlobal -= OnInteractionGlobal;
+            GlobalEventManager.onCharacterDeathGlobal -= OnCharacterDeathGlobal;
+        }
+
+        
         public void SetActive(bool newActive)
         {
             active = newActive;
+        }
+
+        private void FixedUpdate()
+        {
+            if (NetworkServer.active)
+            {
+                if (active)
+                {
+                    float deltaTime = Time.fixedDeltaTime;
+                    AddRage(passiveRageGain * deltaTime);
+                    bool anyPlayersInRadius = CheckPlayersInRadius(deltaTime);
+
+                    if (anyPlayersInRadius && currentTarget != null && currentRage >= maxRage)
+                    {
+                        AmbushActivation(currentTarget);
+                    }
+                }
+            }
+
+            if (debugIndicator)
+            {
+                debugIndicator.localScale = Vector3.one * radius;
+                debugIndicator.gameObject.SetActive(debug && active);
+            }
+        }
+
+        private void AddRage(float rage)
+        {
+            currentRage = Mathf.Clamp(currentRage + rage, 0f, maxRage);
+        }
+        private bool IsPointInRadius(Vector3 position)
+        {
+            Vector3 between = transform.position - position;
+            return between.sqrMagnitude <= (radius * radius);
+        }
+        private bool CheckPlayersInRadius(float deltaTime)
+        {
+            var playerTeamMembers = TeamComponent.GetTeamMembers(TeamIndex.Player);
+            CharacterBody closestPlayer = null;
+            float lowestDistance = Mathf.Infinity;
+            bool anyPlayersInRadius = false;
+            foreach (TeamComponent member in playerTeamMembers)
+            {
+                var playerBody = member.body;
+                if (playerBody && playerBody.isPlayerControlled && IsPointInRadius(playerBody.footPosition))
+                {
+                    anyPlayersInRadius = true;
+
+                    float distance = (transform.position - playerBody.footPosition).magnitude;
+                    if (lowestDistance > distance)
+                    {
+                        lowestDistance = distance;
+                        closestPlayer = member.body;
+                    }
+                }
+            }
+
+            if (anyPlayersInRadius)
+            {
+                float t = Mathf.Clamp01(lowestDistance / radius);
+                float rageGain = Mathf.Lerp(rageGainAtCenter, rageGainAtEdge, t);
+                AddRage(rageGain * deltaTime);
+            }
+
+            if (currentTarget == null || IsPointInRadius(currentTarget.footPosition) == false)
+            {
+                currentTarget = closestPlayer;
+            }
+
+            return anyPlayersInRadius;
+        }
+        private void OnInteractionGlobal(Interactor interactor, IInteractable interactable, GameObject interactableObject)
+        {
+            if (!active)
+            {
+                return;
+            }
+
+            if (interactor.TryGetComponent(out CharacterBody body) && IsPointInRadius(body.footPosition))
+            {
+                currentTarget = body;
+                AddRage(rageOnInteract);
+            }
+        }
+        private void OnCharacterDeathGlobal(DamageReport damageReport)
+        {
+            if (!active)
+            {
+                return;
+            }
+
+            // we dont really care about team. things dying = player is probably doing something
+            if (damageReport.attackerBody && IsPointInRadius(damageReport.attackerBody.footPosition))
+            {
+                currentTarget = damageReport.attackerBody;
+                AddRage(rageOnKill);
+            }
+        }
+
+        private DirectorCard SelectMonsterCard(float monsterCredit, int maxMonsters, int minMonsters)
+        {
+            var weightedSelection = Util.CreateReasonableDirectorCardSpawnList(monsterCredit, maxMonsters, minMonsters);
+            if (weightedSelection.Count == 0)
+            {
+                return null;
+            }
+            return weightedSelection.Evaluate(rng.nextNormalizedFloat);
+        }
+
+        [Server]
+        private void AmbushActivation(CharacterBody activator)
+        {
+            SetActive(false);
+
+            //largeMonsterDirector.currentSpawnTarget = activator.gameObject; // large in middle, small on player
+            smallMonsterDirector.currentSpawnTarget = activator ? activator.gameObject : base.gameObject;
+
+            ActivateCombatDirector(largeMonsterDirector, largeMonsterCredit, chosenLargeCard);
+            ActivateCombatDirector(smallMonsterDirector, smallMonsterCredit, chosenSmallCard);
+
+            var largeMaster = chosenLargeCard.GetSpawnCard().prefab?.GetComponent<CharacterMaster>();
+            CharacterBody largeBody = null;
+            if (largeMaster)
+            {
+                largeBody = largeMaster.bodyPrefab?.GetComponent<CharacterBody>();
+            }
+            var smallMaster = chosenSmallCard.GetSpawnCard().prefab?.GetComponent<CharacterMaster>();
+            CharacterBody smallBody = null;
+            if (smallMaster)
+            {
+                smallBody = smallMaster.bodyPrefab?.GetComponent<CharacterBody>();
+            }
+
+            bool single = smallBody == null || largeBody == null || smallBody.bodyIndex == largeBody.bodyIndex;
+            if (single)
+            {
+                CharacterBody singleBody = smallBody ?? largeBody;
+                if (singleBody)
+                {
+                    Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
+                    {
+                        subjectAsCharacterBody = activator,
+                        baseToken = "SS2_AMBUSH_MESSAGE_SINGLE",
+                        paramTokens = new string[]
+                        {
+                            singleBody.baseNameToken
+                        }
+                    });
+                }
+            }
+            else
+            {
+                Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
+                {
+                    subjectAsCharacterBody = activator,
+                    baseToken = "SS2_AMBUSH_MESSAGE_DOUBLE",
+                    paramTokens = new string[]
+                    {
+                        smallBody.baseNameToken,
+                        largeBody.baseNameToken,
+                    }
+                });
+            }
+
+            EffectManager.SpawnEffect(spawnPositionEffectPrefab, new EffectData
+            {
+                origin = transform.position,
+                rotation = Quaternion.identity,
+                scale = 1f,
+            }, true);
+
+            var effectData = new EffectData
+            {
+                origin = transform.position,
+                rotation = Quaternion.identity,
+                scale = 1f,
+            };
+            effectData.SetNetworkedObjectReference(activator.gameObject);
+            EffectManager.SpawnEffect(activatorEffectPrefab, effectData, true);
+
+        }
+
+        private void ActivateCombatDirector(CombatDirector director, float monsterCredit, DirectorCard directorCard)
+        {
+            director.enabled = true;
+            director.monsterCredit += monsterCredit;
+            director.OverrideCurrentMonsterCard(directorCard);
+            director.monsterSpawnTimer = 0;
         }
 
     }
---
+++ SS2-Project/Assets/Starstorm2/Modules/Interactables/InteractableTypes/Ambush.cs
@@ -0,0 +1,22 @@
+﻿using RoR2;
+using RoR2.ContentManagement;
+using R2API;
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+namespace SS2.Interactables
+{
+    public sealed class Ambush : SS2Interactable
+    {
+        public override SS2AssetRequest<InteractableAssetCollection> AssetRequest => SS2Assets.LoadAssetAsync<InteractableAssetCollection>("acAmbush", SS2Bundle.Interactables);
+        public override void Initialize()
+        {
+
+        }
+        public override bool IsAvailable(ContentPack contentPack)
+        {
+            return SS2Config.enableBeta;
+        }
+
+    }
+}
