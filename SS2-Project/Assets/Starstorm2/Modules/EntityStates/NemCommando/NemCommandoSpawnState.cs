e5b62cb7390f7e3083d7dcbf3db159c661a78451
Hanan Rodebaugh <hananrodebaugh@gmail.com>
2026-04-19 00:58:30 -0400

    animation updates + gunless mode

---
+++ SS2-Project/Assets/Starstorm2/Modules/EntityStates/NemCommando/NemCommandoSpawnState.cs
@@ -0,0 +1,50 @@
+﻿using EntityStates.Generic;
+using RoR2;
+using RoR2.Skills;
+using UnityEngine;
+
+namespace EntityStates.NemCommando
+{
+    public class NemCommandoSpawnState : NemesisSpawnState
+    {
+        public static string portalMuz;
+        public static SkillDef gunSecondary;
+        public static SkillDef gunSpecial;
+        private bool hasGun = true;
+        public static float throwGunDelay;
+        private float throwGunTime;
+        private bool thrownGun = false;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+
+            throwGunTime = throwGunDelay * duration;
+
+            if (skillLocator.secondary.skillDef != gunSecondary && skillLocator.special.skillDef != gunSpecial)
+            {
+                hasGun = false;
+                GetModelAnimator().SetBool("gunEquipped", hasGun);
+                PlayAnimation("FullBody, Override", "SpawnNoGun");
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (!hasGun && !thrownGun && fixedAge >= throwGunTime)
+            {
+                FindModelChild("gunParticle").GetComponent<ParticleSystem>().Emit(1);
+                FindModelChild("GunModel").gameObject.SetActive(false);
+                thrownGun = true;
+            }
+        }
+
+        public override void SpawnEffect()
+        {
+            portalMuzzle = portalMuz;
+            base.SpawnEffect();
+        }
+    }
+}
