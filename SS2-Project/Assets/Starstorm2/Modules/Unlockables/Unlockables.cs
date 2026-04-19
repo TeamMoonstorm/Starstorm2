e72ec188aa7020c51aa0a64f6b65b8d61ccdf07b
warmbuns <anthony@hevia.dev>
2026-04-18 22:25:09 -0700

    why unlock all no work??

--- SS2-Project/Assets/Starstorm2/Modules/Unlockables/Unlockables.cs
+++ SS2-Project/Assets/Starstorm2/Modules/Unlockables/Unlockables.cs
@@ -9,39 +9,95 @@ namespace SS2
 
         internal static void Init()
         {
+            SS2Log.Info($"UnlockAllHandler.Init() called. unlockAll value = {SS2Config.unlockAll.value}");
+
             if (!SS2Config.unlockAll.value)
                 return;
 
-            UnlockableCatalog.availability.onAvailable += BuildCacheAndHook;
+            SS2Log.Info("UnlockAllHandler: Installing hooks...");
+            On.RoR2.UserProfile.HasUnlockable_UnlockableDef += ForceHasUnlockable;
+            On.RoR2.UserProfile.HasSurvivorUnlocked += ForceHasSurvivorUnlocked;
         }
 
-        private static void BuildCacheAndHook()
+        private static void BuildCache()
         {
             ss2Unlockables = new HashSet<UnlockableDef>();
 
-            var unlockables = SS2Content.SS2ContentPack.unlockableDefs;
-            for (int i = 0; i < unlockables.Length; i++)
-            {
-                UnlockableDef def = unlockables[i];
-                if (def == null) continue;
+            var contentPack = SS2Content.SS2ContentPack;
 
-                // skip skins we want players to still earn those
+            SS2Log.Info($"UnlockAllHandler.BuildCache: itemDefs={contentPack.itemDefs.Length}, equipmentDefs={contentPack.equipmentDefs.Length}, survivorDefs={contentPack.survivorDefs.Length}, skillDefs={contentPack.skillDefs.Length}, artifactDefs={contentPack.artifactDefs.Length}, unlockableDefs={contentPack.unlockableDefs.Length}");
+
+            for (int i = 0; i < contentPack.itemDefs.Length; i++)
+            {
+                var def = contentPack.itemDefs[i];
+                if (def && def.unlockableDef)
+                    ss2Unlockables.Add(def.unlockableDef);
+            }
+            for (int i = 0; i < contentPack.equipmentDefs.Length; i++)
+            {
+                var def = contentPack.equipmentDefs[i];
+                if (def && def.unlockableDef)
+                    ss2Unlockables.Add(def.unlockableDef);
+            }
+            for (int i = 0; i < contentPack.survivorDefs.Length; i++)
+            {
+                var def = contentPack.survivorDefs[i];
+                if (def && def.unlockableDef)
+                {
+                    SS2Log.Info($"  Survivor '{def.cachedName}' has unlockableDef '{def.unlockableDef.cachedName}'");
+                    ss2Unlockables.Add(def.unlockableDef);
+                }
+            }
+            for (int i = 0; i < contentPack.skillDefs.Length; i++)
+            {
+                var def = contentPack.skillDefs[i];
+                if (def && def.unlockableDef)
+                    ss2Unlockables.Add(def.unlockableDef);
+            }
+            for (int i = 0; i < contentPack.artifactDefs.Length; i++)
+            {
+                var def = contentPack.artifactDefs[i];
+                if (def && def.unlockableDef)
+                    ss2Unlockables.Add(def.unlockableDef);
+            }
+
+            for (int i = 0; i < contentPack.unlockableDefs.Length; i++)
+            {
+                var def = contentPack.unlockableDefs[i];
+                if (def == null) continue;
                 if (def.cachedName.IndexOf("skin", System.StringComparison.OrdinalIgnoreCase) >= 0)
                     continue;
-
                 ss2Unlockables.Add(def);
             }
 
-            On.RoR2.UserProfile.HasUnlockable_UnlockableDef += ForceHasUnlockable;
-            SS2Log.Info($"UnlockAllHandler: Forcing {ss2Unlockables.Count} SS2 unlockables as unlocked (skins excluded).");
+            SS2Log.Info($"UnlockAllHandler: Built cache with {ss2Unlockables.Count} SS2 unlockables (skins excluded).");
         }
 
         private static bool ForceHasUnlockable(On.RoR2.UserProfile.orig_HasUnlockable_UnlockableDef orig, UserProfile self, UnlockableDef unlockableDef)
         {
-            if (unlockableDef != null && ss2Unlockables.Contains(unlockableDef))
-                return true;
+            if (unlockableDef != null)
+            {
+                if (ss2Unlockables == null)
+                    BuildCache();
 
+                if (ss2Unlockables.Contains(unlockableDef))
+                    return true;
+            }
             return orig(self, unlockableDef);
+        }
+
+        private static bool ForceHasSurvivorUnlocked(On.RoR2.UserProfile.orig_HasSurvivorUnlocked orig, UserProfile self, SurvivorIndex survivorIndex)
+        {
+            SurvivorDef survivorDef = SurvivorCatalog.GetSurvivorDef(survivorIndex);
+            if (survivorDef != null && survivorDef.unlockableDef != null)
+            {
+                if (ss2Unlockables == null)
+                    BuildCache();
+
+                if (ss2Unlockables.Contains(survivorDef.unlockableDef))
+                    return true;
+            }
+            return orig(self, survivorIndex);
         }
     }
 }
