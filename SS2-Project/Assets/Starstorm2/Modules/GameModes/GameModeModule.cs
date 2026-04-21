f4823d21a1d02bb34ae44b0029db67fda261974c
warmbuns <anthony@hevia.dev>
2026-04-21 00:57:25 -0700

    follow chinchi's advice

--- SS2-Project/Assets/Starstorm2/Modules/GameModes/GameModeModule.cs
+++ SS2-Project/Assets/Starstorm2/Modules/GameModes/GameModeModule.cs
@@ -1,49 +1,29 @@
 using R2API;
 using RoR2;
-using RoR2.ContentManagement;
-using RoR2.ExpansionManagement;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.Networking;
 
 namespace SS2
 {
     public static class GameModeModule
     {
-        private static GameObject blitzRunPrefab;
-
         internal static IEnumerator Init()
         {
-        
-            blitzRunPrefab = PrefabAPI.InstantiateClone(new GameObject("BlitzRun"), "BlitzRun");
-            blitzRunPrefab.AddComponent<NetworkIdentity>();
-            PrefabAPI.RegisterNetworkPrefab(blitzRunPrefab);
-
-            // Load ClassicRun to copy field values
             var classicRunPrefab = Addressables.LoadAssetAsync<GameObject>(
                 "RoR2/Base/ClassicRun/ClassicRun.prefab").WaitForCompletion();
-            var classicRun = classicRunPrefab.GetComponent<Run>();
+            var blitzRunPrefab = classicRunPrefab.InstantiateClone("BlitzRun");
 
-            // Add BlitzRun and copy ClassicRun's configuration
-            var blitzRun = blitzRunPrefab.AddComponent<BlitzRun>();
-            blitzRun.nameToken = "SS2_GAMEMODE_BLITZ_NAME";
-            blitzRun.userPickable = true;
-            blitzRun.startingSceneGroup = classicRun.startingSceneGroup;
-            blitzRun.startingScenes = classicRun.startingScenes;
-            blitzRun.gameOverPrefab = classicRun.gameOverPrefab;
-            blitzRun.lobbyBackgroundPrefab = classicRun.lobbyBackgroundPrefab;
-            blitzRun.uiPrefab = classicRun.uiPrefab;
-            blitzRun.rebirthDropTable = classicRun.rebirthDropTable;
+            if (!blitzRunPrefab.TryGetComponent<Run>(out var run))
+            {
+                SS2Log.Error("GameModeModule: Cloned ClassicRun has no Run component.");
+                yield break;
+            }
 
-            blitzRunPrefab.AddComponent<TeamManager>();
-            blitzRunPrefab.AddComponent<NetworkRuleBook>();
-            blitzRunPrefab.AddComponent<TeamFilter>();
-            blitzRunPrefab.AddComponent<EnemyInfoPanelInventoryProvider>();
-            blitzRunPrefab.AddComponent<DirectorCore>();
-            //blitzRunPrefab.AddComponent<ExpansionRequirementComponent>();
-            blitzRunPrefab.AddComponent<RunCameraManager>();
+            run.nameToken = "SS2_GAMEMODE_BLITZ_NAME";
+            run.userPickable = true;
 
+            blitzRunPrefab.AddComponent<BlitzRunBehavior>();
             blitzRunPrefab.AddComponent<GameModeInfo>().buttonHoverDescription = "SS2_GAMEMODE_BLITZ_DESC";
 
             SS2Content.SS2ContentPack.gameModePrefabs.Add(new[] { blitzRunPrefab });
